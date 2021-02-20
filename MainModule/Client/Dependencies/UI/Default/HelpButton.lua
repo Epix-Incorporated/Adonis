dfa95d4edf07e1a74713bbb17645f7bf99e95da8
imskyyc <48039776+imskyyc@users.noreply.github.com>
2021-02-20 06:45:06 -0800

    Update HelpButton.lua

--- MainModule/Client/Dependencies/UI/Default/HelpButton.lua
+++ MainModule/Client/Dependencies/UI/Default/HelpButton.lua
@@ -5,35 +5,29 @@ Routine = nil
 service = nil
 gTable = nil
 
---// All global vars will be wiped/replaced except script
-
 return function(data)
 	local playergui = service.PlayerGui
 	local localplayer = service.Players.LocalPlayer
 	local gui = service.New("ScreenGui")
 	local toggle = service.New("ImageButton", gui)
 	local gTable = client.UI.Register(gui)
-	
+
 	if client.UI.Get("HelpButton", gui, true) then
 		gui:Destroy()
 		gTable:Destroy()
 		return nil
 	end
-	
+
 	gTable.Name = "HelpButton"
 	gTable.CanKeepAlive = false
-	
+
 	toggle.Name = "Toggle"
 	toggle.BackgroundTransparency = 1
-	toggle.Position = UDim2.new(1, -45, 1, -45)
-	toggle.Size = UDim2.new(0, 40, 0, 40)
-	toggle.Image = "rbxassetid://357249130"
-	toggle.ImageTransparency = 0.5
-	
-	--if client.UI.Get("Chat") then
-	--	toggle.Position = UDim2.new(1, -(45+40),1, -45)
-	--end
-	
+	toggle.Position = UDim2.new(1, -60, 1, -60)
+	toggle.Size = UDim2.new(0, 55, 0, 55)
+	toggle.Image = data.Image or "rbxassetid://357249130"
+	toggle.ImageTransparency = 0.2
+
 	toggle.MouseButton1Down:connect(function()
 		local found = client.UI.Get("UserPanel",nil,true)
 		if found then
@@ -42,6 +36,6 @@ return function(data)
 			client.UI.Make("UserPanel",{})
 		end
 	end)
-	
+
 	gTable:Ready()
 end
