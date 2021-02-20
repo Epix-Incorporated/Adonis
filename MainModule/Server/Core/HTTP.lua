a0a581dc0a83f756dee2a43a429cffdfd44944b0
imskyyc <48039776+imskyyc@users.noreply.github.com>
2021-02-20 07:13:04 -0800

    Update HTTP.lua

--- MainModule/Server/Core/HTTP.lua
+++ MainModule/Server/Core/HTTP.lua
@@ -10,7 +10,7 @@ logError = nil
 return function(Vargs)
 	local server = Vargs.Server;
 	local service = Vargs.Service;
-	
+
 	local Functions, Admin, Anti, Core, HTTP, Logs, Remote, Process, Variables, Settings
 	local function Init()
 		Functions = server.Functions;
@@ -23,10 +23,10 @@ return function(Vargs)
 		Process = server.Process;
 		Variables = server.Variables;
 		Settings = server.Settings;
-		
+
 		Logs:AddLog("Script", "HTTP Module Initialized")
 	end;
-	
+
 	server.HTTP = {
 		Init = Init;
 		Service = service.HttpService;
@@ -37,19 +37,9 @@ return function(Vargs)
 			end)
 			if y and not n then return true end
 		end;
-		
-		WebPanel = {
-			Moderators = {};
-			Admins = {};
-			Owners = {};
-			Creators = {};
-			Mutes = {};
-			Bans = {};
-			Blacklist = {};
-			Whitelist = {};
-		};
-		
+
 		Trello = {
+			API = require(server.Deps.TrelloAPI);
 			Helpers = {};
 			Moderators = {};
 			Admins = {};
@@ -63,15 +53,14 @@ return function(Vargs)
 			Blacklist = {};
 			Whitelist = {};
 			PerformedCommands = {};
-			
+
 			Update = function()
-				if not HTTP.CheckHttp() then 
-					--HTPP.Trello.Bans = {'Http is not enabled! Cannot connect to Trello!'}
+				if not HTTP.CheckHttp() and Settings.Trello_Enabled then 
 					warn('Http is not enabled! Cannot connect to Trello!')
-				elseif not Settings.Trello_Enabled then
-					-- Do something else?
-				else
+				elseif Settings.Trello_Enabled then
 					local boards = {}
+					local lists = {}
+					local labels = {}
 					local bans = {}
 					local admins = {}
 					local mods = {}
@@ -85,68 +74,71 @@ return function(Vargs)
 					local perms = {}
 					local blacklist = {}
 					local whitelist = {}
-					
+
 					local function grabData(board)
 						local trello = HTTP.Trello.API(Settings.Trello_AppKey,Settings.Trello_Token)
-						local lists = trello.getLists(board)
-						local banList = trello.getListObj(lists,{"Banlist","Ban List","Bans"})
-						local commandList = trello.getListObj(lists,{"Commands","Command List"})
-						local adminList = trello.getListObj(lists,{"Admins","Admin List","Adminlist"})
-						local modList = trello.getListObj(lists,{"Moderators","Moderator List","Moderatorlist","Modlist","Mod List","Mods"})
-						local creatorList = trello.getListObj(lists,{"Creators","Creator List","Creatorlist","Place Owners"})
-						local ownerList = trello.getListObj(lists,{"Owners","Owner List","Ownerlist"})
-						local musicList = trello.getListObj(lists,{"Music","Music List","Musiclist","Songs"}) 
-						local insertList = trello.getListObj(lists,{"InsertList","Insert List","Insertlist","Inserts","ModelList","Model List","Modellist","Models"}) 
-						local permList = trello.getListObj(lists,{"Permissions","Permission List","Permlist"})
-						local muteList = trello.getListObj(lists,{"Mutelist","Mute List"})
-						local agentList = trello.getListObj(lists,{"Agents","Agent List","Agentlist"})
-						local bList = trello.getListObj(lists,"Blacklist")
-						local wList = trello.getListObj(lists,"Whitelist")
-						
+						local lists = trello.Boards.GetLists(board)
+						local banList = trello.GetListObject(lists,{"Banlist","Ban List","Bans"})
+						local commandList = trello.GetListObject(lists,{"Commands","Command List"})
+						local adminList = trello.GetListObject(lists,{"Admins","Admin List","Adminlist"})
+						local modList = trello.GetListObject(lists,{"Moderators","Moderator List","Moderatorlist","Modlist","Mod List","Mods"})
+						local creatorList = trello.GetListObject(lists,{"Creators","Creator List","Creatorlist","Place Owners"})
+						local ownerList = trello.GetListObject(lists,{"Owners","Owner List","Ownerlist"})
+						local musicList = trello.GetListObject(lists,{"Music","Music List","Musiclist","Songs"}) 
+						local insertList = trello.GetListObject(lists,{"InsertList","Insert List","Insertlist","Inserts","ModelList","Model List","Modellist","Models"}) 
+						local permList = trello.GetListObject(lists,{"Permissions","Permission List","Permlist"})
+						local muteList = trello.GetListObject(lists,{"Mutelist","Mute List"})
+						local agentList = trello.GetListObject(lists,{"Agents","Agent List","Agentlist"})
+						local bList = trello.GetListObject(lists,"Blacklist")
+						local wList = trello.GetListObject(lists,"Whitelist")
+
 						if banList then
-							local cards = trello.getCards(banList.id)
+							local cards = trello.Lists.GetCards(banList.id)
 							for l,k in pairs(cards) do
-								table.insert(bans,k.name)
+								table.insert(bans,{
+									name = k.name;
+									time = k.dateLastUpdate;
+								})
 							end
 						end
-						
+
 						if creatorList then
-							local cards = trello.getCards(creatorList.id)
+							local cards = trello.Lists.GetCards(creatorList.id)
 							for l,k in pairs(cards) do
 								table.insert(creators,k.name)
 							end
 						end
-						
+
 						if modList then
-							local cards = trello.getCards(modList.id)
+							local cards = trello.Lists.GetCards(modList.id)
 							for l,k in pairs(cards) do
 								table.insert(mods,k.name)
 							end
 						end
-						
+
 						if adminList then
-							local cards = trello.getCards(adminList.id)
+							local cards = trello.Lists.GetCards(adminList.id)
 							for l,k in pairs(cards) do
 								table.insert(admins,k.name)
 							end
 						end
-						
+
 						if ownerList then
-							local cards = trello.getCards(ownerList.id)
+							local cards = trello.Lists.GetCards(ownerList.id)
 							for l,k in pairs(cards) do
 								table.insert(owners,k.name)
 							end
 						end
-						
+
 						if agentList then
-							local cards = trello.getCards(agentList.id)
+							local cards = trello.Lists.GetCards(agentList.id)
 							for l,k in pairs(cards) do
 								table.insert(agents,k.name)
 							end
 						end
-						
+
 						if musicList then
-							local cards = trello.getCards(musicList.id)
+							local cards = trello.Lists.GetCards(musicList.id)
 							for l,k in pairs(cards) do
 								if k.name:match('^(.*):(.*)') then
 									local a,b=k.name:match('^(.*):(.*)')
@@ -154,9 +146,9 @@ return function(Vargs)
 								end
 							end
 						end
-						
+
 						if insertList then
-							local cards = trello.getCards(insertList.id)
+							local cards = trello.Lists.GetCards(insertList.id)
 							for l,k in pairs(cards) do
 								if k.name:match('^(.*):(.*)') then
 									local a,b=k.name:match('^(.*):(.*)')
@@ -164,30 +156,30 @@ return function(Vargs)
 								end
 							end
 						end
-						
+
 						if muteList then		
-							local cards = trello.getCards(muteList.id)
+							local cards = trello.Lists.GetCards(muteList.id)
 							for l,k in pairs(cards) do
 								table.insert(mutes,k.name)
 							end
 						end
-						
+
 						if bList then		
-							local cards = trello.getCards(bList.id)
+							local cards = trello.Lists.GetCards(bList.id)
 							for l,k in pairs(cards) do
 								table.insert(blacklist,k.name)
 							end
 						end
-						
+
 						if wList then		
-							local cards = trello.getCards(wList.id)
+							local cards = trello.Lists.GetCards(wList.id)
 							for l,k in pairs(cards) do
 								table.insert(whitelist,k.name)
 							end
 						end
-						
+
 						if permList then
-							local cards = trello.getCards(permList.id)
+							local cards = trello.Lists.GetCards(permList.id)
 							for l,k in pairs(cards) do
 								local com,level = k.name:match("^(.*):(.*)") 
 								if com and level then 
@@ -195,9 +187,9 @@ return function(Vargs)
 								end
 							end
 						end
-						
+
 						if commandList then
-							local cards = trello.getCards(commandList.id)
+							local cards = trello.Lists.GetCards(commandList.id)
 							for l,k in pairs(cards) do
 								if not HTTP.Trello.PerformedCommands[tostring(k.id)] then
 									local cmd = k.name
@@ -215,34 +207,34 @@ return function(Vargs)
 										Desc = cmd;
 									})
 									if Settings.Trello_Token ~= "" then
-										pcall(trello.makeComment,k.id,"Ran Command: "..cmd.."\nPlace ID: "..game.PlaceId.."\nServer Job Id: "..game.JobId.."\nServer Players: "..#service.GetPlayers().."\nServer Time: "..service.GetTime())
+										pcall(trello.Cards.AddComment,k.id,"Ran Command: "..cmd.."\nPlace ID: "..game.PlaceId.."\nServer Job Id: "..game.JobId.."\nServer Players: "..#service.GetPlayers().."\nServer Time: "..service.GetTime())
 									end
 								end
 							end
 						end
 					end
-					
+
 					for i,v in pairs(Settings.Trello_Secondary) do table.insert(boards,v) end
 					if Settings.Trello_Primary~="" then table.insert(boards,Settings.Trello_Primary) end
 					for i,v in pairs(boards) do pcall(grabData,v) end
 					
-					if #bans>0 then HTTP.Trello.Bans = bans end
-					if #creators>0 then HTTP.Trello.Creators = creators end
-					if #admins>0 then HTTP.Trello.Admins = admins end
-					if #mods>0 then HTTP.Trello.Moderators = mods end
-					if #owners>0 then HTTP.Trello.Owners = owners end
-					if #music>0 then HTTP.Trello.Music = music end
-					if #insertlist>0 then HTTP.Trello.InsertList = insertlist end
-					if #mutes>0 then HTTP.Trello.Mutes = mutes end
-					if #agents>0 then HTTP.Trello.Agents = agents end
-					if #blacklist>0 then HTTP.Trello.Blacklist = blacklist end
-					if #whitelist>0 then HTTP.Trello.Whitelist = whitelist end
+					HTTP.Trello.Bans = bans
+					HTTP.Trello.Creators = creators
+					HTTP.Trello.Admins = admins
+					HTTP.Trello.Moderators = mods
+					HTTP.Trello.Owners = owners
+					HTTP.Trello.Music = music
+					HTTP.Trello.InsertList = insertlist 
+					HTTP.Trello.Mutes = mutes
+					HTTP.Trello.Agents = agents
+					HTTP.Trello.Blacklist = blacklist
+					HTTP.Trello.Whitelist = whitelist
 					
 					for i,v in pairs(service.GetPlayers()) do
 						if Admin.CheckBan(v) then
-							v:Kick(Variables.BanMessage)
+							v:Kick(Functions.GetKickMessage("Ban"))
 						end 
-						
+
 						if v and v.Parent then
 							for ind,admin in pairs(HTTP.Trello.Mutes) do
 								if Admin.DoCheck(v,admin) then
@@ -250,17 +242,17 @@ return function(Vargs)
 								end
 							end
 						end
-						
+
 						Admin.UpdateCachedLevel(v)
 					end
-					
+
 					Logs.AddLog(Logs.Script,{
 						Text = "Updated Trello Data";
 						Desc = "Data was retreived from Trello";
 					}) 
 				end
 			end;
-			
+
 			CheckAgent = function(p)
 				for ind,v in pairs(HTTP.Trello.Agents) do
 					if Admin.DoCheck(p,v) then
@@ -269,5 +261,13 @@ return function(Vargs)
 				end
 			end;
 		};
+		
+		GetTrelloBan = function(plr)
+			for _, Ban in pairs(HTTP.Trello.Bans) do
+				if tostring(Ban.name):find(plr.Name) or tostring(Ban.name):find(tostring(plr.UserId)) then
+					return Ban
+				end
+			end
+		end;
 	};
 end
