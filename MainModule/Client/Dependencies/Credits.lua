b23b3b184975fd4c17740270f41106290ec97ae7
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2021-03-12 22:17:26 +0200

    Update Credits.lua

--- MainModule/Client/Dependencies/Credits.lua
+++ MainModule/Client/Dependencies/Credits.lua
@@ -18,9 +18,9 @@ return {
 	{Text = "@GitHub GeneralScripter", 	Desc = "GitHub Contributor"};
 	{Text = "@GitHub moo1210", 			Desc = "GitHub Contributor"};
 	{Text = "@GitHub kent911t", 		Desc = "GitHub Contributor"};
-	{Text = "@GitHub Kan18", 			Desc = "GitHub Contributor"};
 	{Text = "@GitHub crywink", 			Desc = "GitHub Contributor"};
 	{Text = "@GitHub jaydensar", 		Desc = "GitHub Contributor"};
+	{Text = "@GitHub ccuser44",			Desc = "GitHub Contributor"};
 	{Text = "@GitHub Awesomewebm", 		Desc = "GitHub Contributor"};
 	{Text = "@GitHub TheLegendarySpark",Desc = "GitHub Contributor"};
 	{Text = "@GitHub joritochip",		Desc = "GitHub Contributor"};
@@ -30,6 +30,11 @@ return {
 	{Text = "@GitHub enescglyn",		Desc = "GitHub Contributor"};
 	{Text = "@GitHub EpicFazbear",		Desc = "GitHub Contributor"};
 	{Text = "@GitHub p3tray",			Desc = "GitHub Contributor"};
+	{Text = "@GitHub okgabe",			Desc = "GitHub Contributor"};
+	{Text = "@GitHub fxeP1",			Desc = "GitHub Contributor"};
+	{Text = "@GitHub optimisticside",			Desc = "GitHub Contributor"};
+	{Text = "@GitHub NeoInversion",			Desc = "GitHub Contributor"};
+	{Text = "@GitHub imskyyc",			Desc = "GitHub Contributor"};
 	{Text = "",		Desc = "Everyone else"};
 	{Text = "~ EVERYONE ELSE ~",		Desc = ""};
 	{Text = "Stravant/JustAPerson/Jason Priest (?)", Desc = "Wrote LBI (No longer used)"};	
