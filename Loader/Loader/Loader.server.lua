723cbb8f91218cf12903974340888d18b209fd2f
Sceleratis <sceleratis@gmail.com>
2021-05-13 16:00:31 -0400

    Update Loader.server.lua
    Hopefully this fixes https://github.com/Sceleratis/Adonis/issues/338 (?)

--- Loader/Loader/Loader.server.lua
+++ Loader/Loader/Loader.server.lua
@@ -1,8 +1,8 @@
 --[[
-	
+
 	CURRENT LOADER:
 	https://www.roblox.com/library/2373505175/Adonis-Loader-BETA
-	
+
 --]]
 
 
@@ -29,42 +29,42 @@ if _G["__Adonis_MUTEX"] and type(_G["__Adonis_MUTEX"])=="string" then
 	--script:Destroy()
 else
 	_G["__Adonis_MUTEX"] = script:GetFullName()
-	
+
 	local model = script.Parent.Parent
 	local config = model.Config
 	local core = model.Loader
-	
+
 	local dropper = core.Dropper
 	local loader = core.Loader
 	local runner = script
-	
+
 	local settings = config.Settings
 	local plugins = config.Plugins
 	local themes = config.Themes
-	
+
 	local backup = model:Clone()
-	
+
 	local data = {
 		Settings = {};
 		Descriptions = {};
 		ServerPlugins = {};
 		ClientPlugins = {};
 		Themes = {};
-		
+
 		Model = model;
 		Config = config;
 		Core = core;
-		
+
 		Loader = loader;
 		Dopper = dropper;
 		Runner = runner;
-		
+
 		ModuleID = tonumber('23735'..'01710'); --// Trying to break existing (unupdatable) malicious plugins that replace the ModuleID from studio on insertion
 		LoaderID = tonumber('23735'..'05175');
-		
+
 		DebugMode = true;
 	}
-	
+
 	--// Init
 	script:Destroy()
 	model.Name = math.random()
@@ -81,10 +81,11 @@ else
 	local module = require(moduleId)
 	local response = module(data)
 	if response == "SUCCESS" then
-		if (data.Settings and data.Settings.HideScript) and not data.DebugMode then
+		if (data.Settings and data.Settings.HideScript) and not data.DebugMode and not game:GetService("RunService"):IsStudio() then
 			model.Parent = nil
 			game:BindToClose(function() model.Parent = game:GetService("ServerScriptService") model.Name = "Adonis_Loader" end)
 		end
+
 		model.Name = "Adonis_Loader"
 	else
 		error("MainModule failed to load")
@@ -95,26 +96,20 @@ end
 --___________________________________________________________________________________________--
 --___________________________________________________________________________________________--
 --___________________________________________________________________________________________--
---___________________________________________________________________________________________--	
+--___________________________________________________________________________________________--
 
-					___________      .__         .___                   
-					\_   _____/_____ |__|__  ___ |   | ____   ____      
-					 |    __)_\____ \|  \  \/  / |   |/    \_/ ___\     
-					 |        \  |_> >  |>    <  |   |   |  \  \___     
-					/_______  /   __/|__/__/\_ \ |___|___|  /\___  > /\ 
+					___________      .__         .___
+					\_   _____/_____ |__|__  ___ |   | ____   ____
+					 |    __)_\____ \|  \  \/  / |   |/    \_/ ___\
+					 |        \  |_> >  |>    <  |   |   |  \  \___
+					/_______  /   __/|__/__/\_ \ |___|___|  /\___  > /\
 					        \/|__|            \/          \/     \/  \/
 				  --------------------------------------------------------
 				  Epix Incorporated. Not Everything is so Black and White.
 				  --------------------------------------------------------
-				
+
 --___________________________________________________________________________________________--
 --___________________________________________________________________________________________--
 --___________________________________________________________________________________________--
 --___________________________________________________________________________________________--
 																																																							--]]
-
-
-
-
-
-
