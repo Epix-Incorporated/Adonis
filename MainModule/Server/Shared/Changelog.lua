d90c7d9c288c3b65c178072e52b1286b99ba8b2d
Sceleratis <sceleratis@gmail.com>
2021-07-06 17:03:43 -0400

    Update Changelog.lua

--- MainModule/Server/Shared/Changelog.lua
+++ MainModule/Server/Shared/Changelog.lua
@@ -1,10 +1,13 @@
 return {
-	"Version: 221.5";
+	"Version: 221.6";
 	"*Drag edges to expand*";
+	"";
+	"[v221.6 7.6.2021 17:03 EST]";
+	"*CrossServer LoadData no longer triggers LoadData on the origin server (which resulted in lag)";
+	"*Big changes to the datastore; Should improve performance & eliminate throttling";
 	"";
 	"[v221.5 7.6.2021 13:48 EST]";
 	"*Fixed a bug related to startup commands that cause loading to halt which resulted in errors";
-	"*CrossServer LoadData no longer triggers LoadData on the origin server (which resulted in lag)";
 	"";
 	"[v221.4 7.3.2021 21:42 EST]";
 	"*Fixed whitelist-related bug that would break everything else";
