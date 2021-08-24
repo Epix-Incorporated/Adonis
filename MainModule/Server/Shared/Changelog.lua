204e9563b1f32204321db1ab4e169a35e787b271
Sky <sceleratis@gmail.com>
2021-08-24 14:54:01 -0400

    Update Changelog.lua

--- MainModule/Server/Shared/Changelog.lua
+++ MainModule/Server/Shared/Changelog.lua
@@ -4,6 +4,7 @@ return {
 	"";
 	"[v227 WIP]";
 	"*Fixed Window.ScrollingFrame:GenerateList() bugging out on refresh";
+	"*Fixed usage of a non-existant variable in SaveAllPlayerData";
 	"(Git/Expertcoderz) Added !paint #465";
 	"(Git/P3tray) Exploit patch & data.OnClicked functionality for ImageButtons #458";
 	"(Git/fxeP1) Admin/Remote/Process/Service/CRS Updates #457";
