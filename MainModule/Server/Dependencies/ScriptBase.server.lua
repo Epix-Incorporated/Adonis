5f98c4ac4579dc1467564c7acbea83fe7ecca602
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2021-09-23 12:37:30 +0300

    Fixed typo

--- MainModule/Server/Dependencies/ScriptBase.server.lua
+++ MainModule/Server/Dependencies/ScriptBase.server.lua
@@ -2,7 +2,7 @@ wait()
 local execute = script:FindFirstChild("Execute")
 local code, lbi = _G.Adonis.Scripts.ExecutePermission(script, execute and execute.Value)
 
-local canUseLoadstring = loadstring and pcall(loadstring, "local a = 5 loval b = a + 5") or false
+local canUseLoadstring = loadstring and pcall(loadstring, "local a = 5 local b = a + 5") or false
 
 if code then
 	local func = canUseLoadstring and loadstring(code) or lbi(code, getfenv())
