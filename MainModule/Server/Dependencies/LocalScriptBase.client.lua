42387e4826544e7bf323ad077bcd24dfb32ae3e0
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2021-09-23 12:41:13 +0300

    Removed it from the client as having itbon the client is unnecessary as client can never use vanilla loadstring

--- MainModule/Server/Dependencies/LocalScriptBase.client.lua
+++ MainModule/Server/Dependencies/LocalScriptBase.client.lua
@@ -2,10 +2,8 @@ wait()
 local execute = script:FindFirstChild("Execute")
 local code, lbi = _G.Adonis.Scripts.ExecutePermission(script, execute and execute.Value)
 
-local canUseLoadstring = loadstring and pcall(loadstring, "local a = 5 loval b = a + 5") or false
-
 if code then
-	local func = canUseLoadstring and loadstring(code) or lbi(code, getfenv())
+	local func = lbi(code, getfenv())
 	if func then
 		func()
 	end
