46c8744408771064dcba7d1b0ed673df15a544bd
fxeP1 <64731916+fxeP1@users.noreply.github.com>
2021-10-13 23:53:16 +0100

    typo fix

--- MainModule/Server/Commands/Moderators.lua
+++ MainModule/Server/Commands/Moderators.lua
@@ -3142,7 +3142,7 @@ return function(Vargs, env)
 			Function = function(plr,args)
 				for i,v in pairs(service.GetPlayers(plr,args[1])) do
 					if v.Character then
-						local hum = v.Character:FindFirstChildWhichOfClass("Humanoid")
+						local hum = v.Character:FindFirstChildOfClass("Humanoid")
 						if hum then
 							hum.Health = 0
 						end
