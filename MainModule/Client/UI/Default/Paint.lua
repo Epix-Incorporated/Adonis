56f21b60887aa736b7dc004ce15e4d2e843a34cc
Expertcoderz <81153405+Expertcoderz@users.noreply.github.com>
2021-12-21 12:33:59 +0800

    Update Paint.lua

--- MainModule/Client/UI/Default/Paint.lua
+++ MainModule/Client/UI/Default/Paint.lua
@@ -82,6 +82,89 @@ return function(data)
 		Position = UDim2.new(0, 5, 0, 4);
 		BackgroundTransparency = 0.25;
 		TextXAlignment = "Left";
+client, service = nil, nil
+
+return function(data)
+	local mouse = service.Players.LocalPlayer:GetMouse()
+	local hold = false
+
+	local window = client.UI.Make("Window", {
+		Name  = "Paint";
+		Title = "Paint";
+		Icon = client.MatIcons.Palette;
+	})
+
+	local topbar = window:Add("Frame", {
+		Size = UDim2.new(1, 0, 0, 28);
+		Position = UDim2.new(0, 0, 0, 0);
+		BackgroundTransparency = 1;
+	})
+
+	local pixelCount = topbar:Add("TextLabel", {
+		Size = UDim2.new(0, 100, 1, -8);
+		Position = UDim2.new(1, -180, 0, 4);
+		Text = "Pixels: 0";
+		TextXAlignment = "Right";
+		BackgroundTransparency = 1;
+	})
+
+	local canvas = window:Add("ScrollingFrame", {
+		Position = UDim2.new(0, 0, 0, 30);
+		Size = UDim2.new(1, 0, 1, -30);
+		BackgroundColor3 = Color3.new(1, 1, 1);
+		ClipsDescendants = true;
+	})
+	
+	local pointer = canvas:Add("Frame", {
+		Position = UDim2.new(0, 0, 0, 0);
+		Size = UDim2.new(0, 4, 0, 4);
+		BackgroundColor3 = Color3.new(0, 0, 0);
+		BackgroundTransparency = 0;
+		Visible = false;
+	})
+
+	canvas.MouseEnter:Connect(function()
+		pointer.Visible = true
+	end)
+
+	canvas.MouseLeave:Connect(function()
+		pointer.Visible = false
+	end)
+
+	canvas.MouseMoved:Connect(function(x, y)
+		local offset = Vector2.new(math.abs(x - canvas.AbsolutePosition.X), math.abs(y - canvas.AbsolutePosition.Y - 36))
+		pointer.Position = UDim2.new(0, offset.X, 0, offset.Y)
+
+		if hold == false then return end
+
+		local pixel = pointer:Clone()
+		pixel.Name = "Pixel"
+		pixel.Parent = canvas
+		pixelCount.Text = "Pixels: "..tostring(pixelCount.Text:sub(9) + 1)
+	end)
+	
+	local clearButton = topbar:Add("TextButton", {
+		Size = UDim2.new(0, 70, 1, -8);
+		Position = UDim2.new(1, -75, 0, 4);
+		Text = "Clear";
+		TextXAlignment = "Center";
+		OnClick = function()
+			local children = canvas:GetChildren()
+			for i, child in pairs (children) do
+				if child.Name == "Pixel" then
+					child:Destroy()
+				end
+			end
+			pixelCount.Text = "Pixels: 0"
+		end
+	})
+
+	topbar:Add("TextLabel", {
+		Text = " Color: ";
+		Size = UDim2.new(0, 90, 1, -8);
+		Position = UDim2.new(0, 5, 0, 4);
+		BackgroundTransparency = 0.25;
+		TextXAlignment = "Left";
 		Children = {
 			{
 				Class = "TextButton";
@@ -121,11 +204,11 @@ return function(data)
 				TextXAlignment = "Right";
 				ClipsDescendants = true;
 				TextChanged = function(text, enter, new)
-					if enter and tonumber(text) then
+					if tonumber(text) then
 						if tonumber(text) < 100 then
-							pointer.Size = UDim2.new(0, text, 0, text);
+							pointer.Size = UDim2.new(0, text, 0, text)
 						else
-							pointer.Size = UDim2.new(0, 99, 0, 99);
+							pointer.Size = UDim2.new(0, 99, 0, 99)
 						end
 					end
 				end
@@ -146,3 +229,4 @@ return function(data)
 		end
 	end)
 end
+
