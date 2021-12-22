f14d6366bef013cf417dad2c99f0472683b468f2
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2021-12-22 13:14:05 +0200

    Update Core.lua

--- MainModule/Client/Core/Core.lua
+++ MainModule/Client/Core/Core.lua
@@ -315,7 +315,7 @@ return function()
 				}, nil, nil, true);
 			}
 
-			local AdonisGTable = NewProxy{
+			local AdonisGTable = NewProxy({
 				__index = function(tab,ind)
 					if ind == "Scripts" then
 						return API.Scripts
@@ -333,13 +333,23 @@ return function()
 					error("Read-only")
 				end;
 				__metatable = "API";
-			}
+			})
 
-			if not rawget(_G,"Adonis") then
-				rawset(_G,"Adonis",AdonisGTable)
-				StartLoop("APICheck",1,function()
-					rawset(_G,"Adonis",AdonisGTable)
-				end, true)
+			if not rawget(_G, "Adonis") then
+				if table.isfrozen and not table.isfrozen(_G) or not table.isfrozen then
+					rawset(_G, "Adonis", AdonisGTable)
+					StartLoop("APICheck", 1, function()
+						if not rawget(_G, "Adonis") == AdonisGTable then
+							if table.isfrozen and not table.isfrozen(_G) or not table.isfrozen then
+								rawset(_G, "Adonis", AdonisGTable)
+							else
+								warn("ADONIS CRITICAL WARNING! MALICIOUS CODE IS TRYING TO CHANGE THE ADONIS _G API AND IT CAN'T BE SET BACK! PLEASE SHUTDOWN THE SERVER AND REMOVE THE MALICIOUS CODE IF POSSIBLE!")
+							end
+						end
+					end, true)
+				else
+					warn("The _G table was locked and the Adonis _G API could not be loaded")
+				end
 			end
 		end;
 	};
