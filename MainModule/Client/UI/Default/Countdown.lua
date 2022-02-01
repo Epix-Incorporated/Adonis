cfd34c329e5e4194987e40426dd24b565a522980
Expertcoderz <81153405+Expertcoderz@users.noreply.github.com>
2022-02-01 16:18:21 +0800

    Add !countdown; elapsed time display in Countdown UI (#683)
    * Add !countdown command for players
    
    * Add elapsed time display; some code improvements

--- MainModule/Client/UI/Default/Countdown.lua
+++ MainModule/Client/UI/Default/Countdown.lua
@@ -1,27 +1,22 @@
-
-client = nil
-service = nil
+client, service = nil, nil
 
 return function(data)
 	local gTable
 	local tLimit = data.Time
 	local sImg
-	
+
 	local isMuted = false
-	
-	local tock = service.New('Sound')
-	tock.Volume = 0.25
-	tock.Looped = false
-	tock.SoundId = 'http://www.roblox.com/asset/?id=151715959'
-	
-	local buzzer = service.New('Sound')
-	buzzer.Volume = 0.25
-	buzzer.Looped = false
-	buzzer.SoundId = 'http://www.roblox.com/asset/?id=267883066'
-	
+
+	local tock = service.New("Sound", {
+		Volume = 0.25; Looped = false; SoundId = "rbxassetid://151715959";
+	})
+	local buzzer = service.New("Sound", {
+		Volume = 0.25; Looped = false; SoundId = "rbxassetid://267883066";
+	})
+
 	local textSize = service.TextService:GetTextSize(tLimit, 100, Enum.Font.SourceSans, Vector2.new(math.huge,math.huge))	
 	if textSize.X < 150 then textSize = Vector2.new(175, textSize.Y) end
-	
+
 	local window = client.UI.Make("Window", {
 		Name = "Countdown";
 		Title = "Countdown";
@@ -35,13 +30,21 @@ return function(data)
 			buzzer:Destroy()
 		end
 	})
-	
+
 	local label = window:Add("TextLabel", {
 		Text = tLimit;
 		BackgroundTransparency = 1;
 		TextScaled = true;
 	})
-	
+
+	local elapsed = window:Add("TextLabel", {
+		Text = "0";
+		TextXAlignment = Enum.TextXAlignment.Right;
+		BackgroundTransparency = 1;
+		Size = UDim2.fromOffset(25, 25);
+		Position = UDim2.new(1, -30, 1, -30);
+	})
+
 	local muteButton = window:AddTitleButton({
 		Text = "";
 		OnClick = function()
@@ -53,12 +56,12 @@ return function(data)
 			else
 				tock.Volume = 0
 				buzzer.Volume = 0
-				sImg.Image = "rbxassetid://1638584675";
+				sImg.Image = "rbxassetid://1638584675"
 				isMuted = true
 			end
 		end
 	})
-	
+
 	sImg = muteButton:Add("ImageLabel", {
 		Size = UDim2.new(1,0,1,0);
 		Position = UDim2.new(0,0,0,0);
@@ -66,45 +69,43 @@ return function(data)
 		Image = "rbxassetid://1638551696";
 		BackgroundTransparency = 1;
 	})
-	
+
 	tock.Parent = label
 	buzzer.Parent = label
 	gTable = window.gTable
 	gTable:Ready()
-	
+
 	local startTime = os.clock()
 	local expectedDelay = 0
-	local waitTime = 1
 	local timeOff = 0
-	
+
 	for i = tLimit, 1, -1 do
-		if gTable.Active then
-			tock:Play()
-			label.Text = i
-		else
-			break
-		end
-		wait(waitTime - timeOff)
-		expectedDelay += waitTime
+		if not gTable.Active then break end
+		tock:Play()
+		label.Text = i
+		elapsed.Text = tLimit - i
+		wait(1 - timeOff)
+		expectedDelay += 1
 		timeOff = os.clock() - startTime - expectedDelay
 	end
-	
+
 	label.Text = "0"
-	
+	elapsed.Text = tLimit
+
 	buzzer:Play()
-	
-	for k = 0,3 do
+
+	for k = 0, 3 do
 		buzzer:Play()
 		for i = 1, 0, -0.1 do
 			label.TextTransparency = i
 			wait(0.05)
 		end
-		
+
 		for i = 0, 1, 0.1 do
 			label.TextTransparency = i
 			wait(0.05)
 		end
 	end
-	
+
 	window:Close()
 end
