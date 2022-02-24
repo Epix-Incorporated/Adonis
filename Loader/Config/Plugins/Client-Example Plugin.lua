4e95d54a49552a8ca23163a0c9f92ecfc62ccd7b
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2022-02-24 11:42:11 +0200

    Update Client-Example Plugin.lua

--- Loader/Config/Plugins/Client-Example Plugin.lua
+++ Loader/Config/Plugins/Client-Example Plugin.lua
@@ -1,5 +1,6 @@
---!nolint UnknownGlobal
-return function()
+return function(Vargs)
+	local client, service = Vargs.Client, Vargs.Service
+
 	--Acts the same as a server plugin but with client functions instead of server.
 	--[[
 	local window = client.UI.Make("Window",{
