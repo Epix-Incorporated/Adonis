5c9d369a718261b16801b554168f4e1853a5de2a
Sceleratis <sceleratis@gmail.com>
2022-04-03 15:08:55 -0400

    Find existing cameras & waypoints
    Object names must be prepended with Adonis_ to ensure we don't find things we aren't supposed to be using.
    
    Adonis_Camera: CameraName
    Adonis_Waypoint: WaypointName
    
    This functionality was removed at some point, this commit adds it back.

---
+++ MainModule/Server/Plugins/FindExistingObjects.lua
@@ -0,0 +1,32 @@
+--[[
+    Description: Searches the place for existing Adonis items and registers them.
+    Author: Sceleratis
+    Date: 4/3/2022
+--]]
+
+return function(Vargs)
+	local server = Vargs.Server;
+	local service = Vargs.Service;
+
+	local Settings = server.Settings
+	local Functions, Commands, Admin, Anti, Core, HTTP, Logs, Remote, Process, Variables, Deps =
+		server.Functions, server.Commands, server.Admin, server.Anti, server.Core, server.HTTP, server.Logs, server.Remote, server.Process, server.Variables, server.Deps
+
+    local objNamePrepend = "Adonis_"
+
+    for i,child in ipairs(workspace:GetDescendants()) do
+        local type,name = string.match(child.Name, "Adonis_(.*): (.*)")
+        if child:IsA("BasePart") and type and name then
+            if string.lower(type) == "camera" then
+                table.insert(Variables.Cameras, { Brick = child, Name = name })
+            elseif string.lower(type) == "waypoint" then
+                Variables.Waypoints[name] = child.Position
+            end
+            
+            Logs.AddLog("Script", {
+                Text = "Found ".. child.Name;
+                Desc = "Found and registered system object of type '".. type .."' with name '".. name .."'";
+            })
+        end
+    end
+end
