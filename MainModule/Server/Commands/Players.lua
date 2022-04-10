3888df5edc4cef27db47ac9b41d93a237e8f60dc
P3tray <31691973+P3tray@users.noreply.github.com>
2022-04-10 15:50:23 +0100

    add a missing semicolon

--- MainModule/Server/Commands/Players.lua
+++ MainModule/Server/Commands/Players.lua
@@ -1081,7 +1081,7 @@ return function(Vargs, env)
 		
 		AudioPlayer = {
 			Prefix = Settings.PlayerPrefix;
-			Commands = {"ap", "audioplayer", "mp", "musicplayer"}
+			Commands = {"ap", "audioplayer", "mp", "musicplayer"};
 			Args = {"soundId?"};
 			Description = "Opens the audio player";
 			AdminLevel = "Players";
