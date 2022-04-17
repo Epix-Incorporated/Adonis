0402aebb5af9748e452d40ed85d4a87cb7abd3fe
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2022-04-17 17:48:43 +0300

    Update Anti.lua

--- MainModule/Client/Core/Anti.lua
+++ MainModule/Client/Core/Anti.lua
@@ -668,7 +668,7 @@ return function()
 
 				-- // Checks for certain disallowed object names in the core GUI which wouldnt otherwise be detectable
 				for _, v in pairs({"SentinelSpy", "ScriptDumper", "VehicleNoclip", "Strong Stand"}) do -- recursive findfirstchild check that yeets some stuff; --[["Sentinel",]]
-					local object = Player and Player.Name ~= v and rawGame.FindFirstChild(rawGame, v, true)            -- ill update the list periodically
+					local object = Player and Player.Name ~= v and service.UnWrap(game).FindFirstChild(service.UnWrap(game), v, true)            -- ill update the list periodically
 					if object then
 						Detected("kick", "Malicious Object?: " .. v)
 					end
