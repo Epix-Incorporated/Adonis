dd24a2cd86799d8b86e30481a8d815c3dafbb633
Madi <49875377+L8X@users.noreply.github.com>
2022-04-19 05:21:19 +0100

    Added more services to server ServicesWeUse (#740)

--- MainModule/Server/Server.lua
+++ MainModule/Server/Server.lua
@@ -73,11 +73,15 @@ local ServicesWeUse = {
 	"SoundService";
 	"StarterGui";
 	"StarterPack";
-	"StarterPlayers";
+	"StarterPlayer"; 
+        "GroupService";
+        "MarketplaceService";
+        "MarketplaceService";
 	"TestService";
 	"HttpService";
+        "RunService";
 	"InsertService";
-	"NetworkServer"
+	"NetworkServer";
 }
 
 local unique = {}
