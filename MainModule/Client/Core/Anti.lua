2e07d488b59287a06ee9c96150d41fff9cee4eae
Expertcoderz <81153405+Expertcoderz@users.noreply.github.com>
2022-05-27 17:58:17 +0800

    Update Anti.lua

--- MainModule/Client/Core/Anti.lua
+++ MainModule/Client/Core/Anti.lua
@@ -238,7 +238,7 @@ return function(Vargs, GetEnv)
 
 					if
 						success or err ~= "Expected ':' not '.' calling member function Kick" or
-						success2 or string.match(err2, "Kick is not a valid member of Workspace \"(.+)\"") ~= workspace.Name
+						success2 or string.match(err2, "^Kick is not a valid member of Workspace \"(.+)\"$") ~= workspace.Name
 					then
 						Detected("kick", "Anti kick found! Method 1")
 					end
