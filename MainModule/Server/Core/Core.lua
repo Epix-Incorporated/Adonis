1282a3548ec6776aec072ba46dab27739ab7970c
P3tray <31691973+P3tray@users.noreply.github.com>
2022-06-13 12:14:48 +0000

    Mistakes where made
    So sorry

--- MainModule/Server/Core/Core.lua
+++ MainModule/Server/Core/Core.lua
@@ -1133,7 +1133,7 @@ return function(Vargs, GetEnv)
 				local SavedSettings
 				local SavedTables
 				if Core.DataStore and Settings.DataStoreEnabled then
-					if Settings.DataStoreKey == server.Defaults.Settings.DataStoreKey or true then
+					if Settings.DataStoreKey == server.Defaults.Settings.DataStoreKey then
 						table.insert(server.Messages, {
 							Title = "Warning!";
 							Message = "Using default datastore key!";
