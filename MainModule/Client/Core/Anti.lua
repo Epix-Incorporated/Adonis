eab93bc4b9d07f46cce7e50061b872222c17069e
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2022-06-16 11:20:40 +0300

    Update Anti.lua

--- MainModule/Client/Core/Anti.lua
+++ MainModule/Client/Core/Anti.lua
@@ -666,7 +666,8 @@ return function(Vargs, GetEnv)
 				end
 
 				-- // Checks for anti-coregui detetection bypasses
-				xpcall(function()
+				-- It's not physically possible for this code to error, yet it still does, weird
+				--[[xpcall(function()
 					local testDecal = service.UnWrap(Instance.new("Decal"))
 					testDecal.Texture = "rbxasset://textures/face.png" -- Its a local asset and it's probably likely to never get removed, so it will never fail to load, unless the users PC is corrupted
 					local activated = false
@@ -683,7 +684,7 @@ return function(Vargs, GetEnv)
 					end
 				end, function()
 					Detected("kick", "Tamper Protection 568234")
-				end)
+				end)]]
 
 				-- // Checks disallowed content URLs in the CoreGui
 				xpcall(function()
