dd5d37f9d8b7fdd31f375490cabfecfb0c07cdf9
alau740 <77434904+alau740@users.noreply.github.com>
2022-06-18 23:07:04 +0100

    fun commands cmd module split non admins

--- MainModule/Server/Commands/Fun.lua
+++ MainModule/Server/Commands/Fun.lua
@@ -168,33 +168,6 @@ return function(Vargs, env)
 			end
 		};
 
-		wat = { --// wat??
-			Prefix = "!";
-			Commands = {"wat"};
-			Args = {};
-			Hidden = true;
-			Description = "???";
-			Fun = true;
-			AdminLevel = "Players";
-			Function = function(plr: Player, args: {string})
-				local wot = {3657191505, 754995791, 160715357, 4881542521, 227499602, 217714490, 130872377, 142633540, 259702986, 6884041159}
-				Remote.Send(plr, "Function", "PlayAudio", wot[math.random(1,#wot)])
-			end
-		};
-
-		YouBeenTrolled = {
-			Prefix = "?";
-			Commands = {"trolled", "freebobuc", "freedonor", "adminpls", "enabledonor"};--//add more :)
-			Args = {};
-			Fun = true;
-			Hidden = true;
-			Description = "You've Been Trolled You've Been Trolled Yes You've Probably Been Told...";
-			AdminLevel = "Players";
-			Function = function(plr: Player, args: {string})
-				Remote.MakeGui(plr, "Effect", {Mode = "trolling";})
-			end
-		};
-
 		Trigger = {
 			Prefix = Settings.Prefix;
 			Commands = {"trigger"};
@@ -812,19 +785,6 @@ return function(Vargs, env)
 			end
 		};
 
-		iloveyou = {
-			Prefix = "?";
-			Commands = {"iloveyou", "alwaysnear", "alwayswatching"};
-			Args = {};
-			Fun = true;
-			Hidden = true;
-			Description = "I love you. You are mine. Do not fear; I will always be near.";
-			AdminLevel = "Players";
-			Function = function(plr: Player, args: {string})
-				Remote.MakeGui(plr, "Effect", {Mode = "lifeoftheparty";})
-			end
-		};
-
 		Blind = {
 			Prefix = Settings.Prefix;
 			Commands = {"blind"};
@@ -1324,40 +1284,6 @@ return function(Vargs, env)
 							end
 						end
 					end
-				end
-			end
-		};
-
-		Pets = {
-			Prefix = Settings.PlayerPrefix;
-			Commands = {"pets"};
-			Args = {"follow/float/swarm/attack", "player"};
-			Hidden = false;
-			Description = "Makes your hat pets do the specified command (follow/float/swarm/attack)";
-			Fun = true;
-			AdminLevel = "Players";
-			Function = function(plr: Player, args: {string})
-				local hats = plr.Character:FindFirstChild("ADONIS_HAT_PETS")
-				if hats then
-					for i, v in pairs(service.GetPlayers(plr, args[2])) do
-						if v.Character:FindFirstChild("HumanoidRootPart") and v.Character.HumanoidRootPart:IsA("Part") then
-							if args[1]:lower() == "follow" then
-								hats.Mode.Value = "Follow"
-								hats.Target.Value = v.Character.HumanoidRootPart
-							elseif args[1]:lower() == "float" then
-								hats.Mode.Value = "Float"
-								hats.Target.Value = v.Character.HumanoidRootPart
-							elseif args[1]:lower() == "swarm" then
-								hats.Mode.Value = "Swarm"
-								hats.Target.Value = v.Character.HumanoidRootPart
-							elseif args[1]:lower() == "attack" then
-								hats.Mode.Value = "Attack"
-								hats.Target.Value = v.Character.HumanoidRootPart
-							end
-						end
-					end
-				else
-					Functions.Hint("You don't have any hat pets! If you are an admin use the :hatpets command to get some", {plr})
 				end
 			end
 		};
