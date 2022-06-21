336439d03f6c46757c217bc399ddfefa7e01f82f
DimensionalFusion <64012878+pbstFusion@users.noreply.github.com>
2022-06-21 15:57:39 +0100

    Update BadQuotes.lua

--- MainModule/Server/Dependencies/Assets/BadQuotes.lua
+++ MainModule/Server/Dependencies/Assets/BadQuotes.lua
@@ -1,4 +1,4 @@
 return {
-    [['"You're entitled to your own opinion, even if it is wrong."']];
-    [["When life gives you lemons, burn down life's house." - CJ']];
+    [["You're entitled to your own opinion, even if it is wrong."]];
+    [["When life gives you lemons, burn down life's house." - CJ]];
 }
