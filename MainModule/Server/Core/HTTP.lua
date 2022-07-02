f7b482d9be0b72acc57142d919d112824efe6742
Expertcoderz <81153405+Expertcoderz@users.noreply.github.com>
2022-07-02 15:16:36 +0800

    Fix typo (oops)

--- MainModule/Server/Core/HTTP.lua
+++ MainModule/Server/Core/HTTP.lua
@@ -41,7 +41,7 @@ return function(Vargs, GetEnv)
 	server.HTTP = {
 		Init = Init;
 		HttpEnabled = (function()
-			local sucess, res = pcall(service.HttpService.GetAsync, service.HttpService, "https://google.com/robots.txt")
+			local success, res = pcall(service.HttpService.GetAsync, service.HttpService, "https://google.com/robots.txt")
 			if not success and res:find("Http requests are not enabled.") then
 				return false
 			end
