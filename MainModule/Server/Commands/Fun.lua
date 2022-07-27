0efc95f48d1dd4fadc9420f4c534b112d07e1fd5
DimensionalFusion <64012878+pbstFusion@users.noreply.github.com>
2022-07-27 13:13:31 +0100

    Change Set FPS to not exceed 60

--- MainModule/Server/Commands/Fun.lua
+++ MainModule/Server/Commands/Fun.lua
@@ -122,8 +122,10 @@ return function(Vargs, env)
 			AdminLevel = "Admins";
 			Function = function(plr: Player, args: {string})
 				local value = assert(tonumber(args[2]), "Missing/invalid FPS value (argument #2)")
-				for _, v in service.GetPlayers(plr, args[1]) do
-					Remote.Send(v, "Function", "SetFPS", value)
+				if value > 60 then assert("2520 FPS cannot exceed 60!") else
+					for _, v in service.GetPlayers(plr, args[1]) do
+						Remote.Send(v, "Function", "SetFPS", value)
+					end
 				end
 			end
 		};
