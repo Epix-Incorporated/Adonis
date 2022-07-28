951d9d38070d3319c286179e197dc8db1f02a04f
DimensionalFusion <64012878+pbstFusion@users.noreply.github.com>
2022-07-28 09:50:33 +0100

    Create FPS UI

---
+++ MainModule/Client/UI/Default/FPS.lua
@@ -0,0 +1,43 @@
+client, service = nil, nil
+
+return function(data, env)
+	if env then
+		setfenv(1, env)
+	end
+	
+	local measuring = true
+	local gTable
+	
+	local window = client.UI.Make("Window",{
+		Name  = "FPS";
+		Title = "FPS";
+		Icon = client.MatIcons.Leaderboard;
+		Size  = {150, 70};
+		Position = UDim2.new(0, 10, 1, -80);
+		AllowMultiple = false;
+		NoHide = true;
+		OnClose = function()
+			measuring = false
+		end
+	})
+	
+	if window then
+		local label = window:Add("TextLabel",{
+			Text = "...";
+			BackgroundTransparency = 1;
+			TextSize = 20;
+			Size = UDim2.new(1, 0, 1, 0);
+			Position = UDim2.new(0, 0, 0, 0);
+			--TextScaled = true;
+			--TextWrapped = true;
+		})
+		
+		gTable = window.gTable
+		window:Ready()
+		
+		repeat
+			label.Text = client.Remote.FPS().." Frames"
+			wait(2)
+		until not measuring or not gTable.Active
+	end
+end
