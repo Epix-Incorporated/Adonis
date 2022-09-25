fb90490d7cf11b3a731aebac58f3573ad97a9499
Expertcoderz <81153405+Expertcoderz@users.noreply.github.com>
2022-09-26 02:58:35 +0800

    Add leading comment block & --!nocheck

--- MainModule/Client/Client.lua
+++ MainModule/Client/Client.lua
@@ -1,6 +1,14 @@
 -------------------
 -- Adonis Client --
 -------------------
+--!nocheck
+																																																																																						  --[[
+This module is part of Adonis 1.0 and contains lots of old code;
+future updates will generally only be made to fix bugs, typos or functionality-affecting problems.
+
+If you find bugs or similar issues, please submit an issue report
+on our GitHub repository here: https://github.com/Epix-Incorporated/Adonis/issues/new/choose
+																																																																																						]]
 
 --// Load Order List
 local CORE_LOADING_ORDER = table.freeze {
