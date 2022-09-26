268b5b6d996df06ae883bf346797a2612777ce12
Expertcoderz <81153405+Expertcoderz@users.noreply.github.com>
2022-09-26 16:23:51 +0800

    Fix syntax error (caused by a missing parenthesis)

--- MainModule/Server/Commands/Moderators.lua
+++ MainModule/Server/Commands/Moderators.lua
@@ -6254,7 +6254,7 @@ return function(Vargs, env)
 						Time = v.Time;
 						Text = v.Text..": "..tostring(v.Desc);
 						Desc = tostring(v.Desc);
-					}
+					})
 				end
 				return tab
 			end;
