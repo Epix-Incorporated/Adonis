c7db51c32e38dc420aea503607c9e8823a2d725e
fxeP1 <64731916+fxeP1@users.noreply.github.com>
2022-10-11 02:37:14 +0100

    Track (all/persistent) fix + Minor code cleanup
    `Core`:
    Cleanup for junk data removal.
    
    `Server`:
    Remove CloneTable duplicate reference
    Set CloneTable to use the Server.lua version, shouldn't effect anything (hopefully?, we may have issues with metatables?)
    
    `Moderators`:
    Fix typo
    Track fix

--- MainModule/Server/Server.lua
+++ MainModule/Server/Server.lua
@@ -533,20 +533,6 @@ return service.NewProxy({
 		local setTab = require(server.Deps.DefaultSettings)
 		server.Defaults = setTab
 		server.Settings = data.Settings or setTab.Settings or {}
-		-- For some reason line 540 errors because CloneTable is nil
-		local function CloneTable(tab, recursive)
-			local clone = table.clone(tab)
-
-			if recursive then
-				for i, v in pairs(clone) do
-					if type(v) == "table" then
-						clone[i] = CloneTable(v, recursive)
-					end
-				end
-			end
-
-			return clone
-		end
 		server.OriginalSettings = CloneTable(server.Settings, true)
 		server.Descriptions = data.Descriptions or setTab.Descriptions or {}
 		server.Messages = data.Messages or setTab.Settings.Messages or {}
@@ -650,6 +636,7 @@ return service.NewProxy({
 
 		--// Server Specific Service Functions
 		ServiceSpecific.GetPlayers = server.Functions.GetPlayers
+		ServiceSpecific.CloneTable = CloneTable
 
 		--// Initialize Cores
 		local runLast = {}
