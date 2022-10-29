99461b29a9604cb567bd9e9d3ba903b27730c8da
Fungus Generator <112895605+FungusGenerator@users.noreply.github.com>
2022-10-29 12:51:51 +0300

    Fix :bot
    This change fixes the issue of "walk" always being true and the bot number always being 50 or higher on the :bot command.

--- MainModule/Server/Commands/Moderators.lua
+++ MainModule/Server/Commands/Moderators.lua
@@ -6627,13 +6627,19 @@ return function(Vargs, env)
 			Description = "AI bots made for training; ':bot scel 5 true true'";
 			AdminLevel = "Moderators";
 			Function = function(plr: Player, args: {string})
-				local num = tonumber(args[2]) and math.max(tonumber(args[2]), 50) or 1
+				local num = tonumber(args[2]) and math.min(tonumber(args[2]), 50) or 1
 				local health = tonumber(args[6]) or 100
 				local speed = tonumber(args[7]) or 16
 				local damage = tonumber(args[8]) or 5
-				local walk = args[3] == "false" and false or true
 				local attack = args[4] == "true" and true or false
 				local friendly = args[5] == "true" and true or false
+				local walk
+				
+				if args[3] == "false" then
+					walk = false
+				else
+					walk = true
+				end
 
 				for _, v in service.GetPlayers(plr, args[1]) do
 					Functions.makeRobot(v, num, health, speed, damage, walk, attack, friendly)
