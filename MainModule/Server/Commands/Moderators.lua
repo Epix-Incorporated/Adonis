05a334e4e969dcb45798642cc91e258ab79572db
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2022-12-22 15:02:19 +0200

    Update Moderators.lua

--- MainModule/Server/Commands/Moderators.lua
+++ MainModule/Server/Commands/Moderators.lua
@@ -180,11 +180,6 @@ return function(Vargs, env)
 						Time = math.round(num);
 					})
 				end
-				--for i = num, 1, -1 do
-				--Functions.Message("Countdown", tostring(i), service.Players:GetPlayers(), false, 1.1)
-				--Functions.Message(" ", i, false, service.Players:GetPlayers(), 0.8)
-				--wait(1)
-				--end
 			end
 		};
 
@@ -2031,7 +2026,7 @@ return function(Vargs, env)
 				local responses = {}
 				local voteKey = "ADONISVOTE".. math.random();
 				local players = service.GetPlayers(plr, args[1])
-				local startTime = os.time();
+				local startTime = os.clock();
 
 				local function voteUpdate()
 					local total = #responses
@@ -2041,15 +2036,15 @@ return function(Vargs, env)
 						"Question: "..question;
 						"Total Responses: "..total;
 						"Didn't Vote: "..#players-total;
-						"Time Left: ".. math.max(0, 120 - (os.time()-startTime));
+						"Time Left: ".. math.max(0, 120 - (os.clock()-startTime));
 					}
 
-					for i, v in responses do
+					for _, v in responses do
 						if not results[v] then results[v] = 0 end
 						results[v] += 1
 					end
 
-					for i, v in anstab do
+					for _, v in anstab do
 						local ans = v
 						local num = results[v]
 						local percent
@@ -2094,57 +2089,6 @@ return function(Vargs, env)
 				})
 
 				delay(120, function() Logs.TempUpdaters[voteKey] = nil end)
-				--[[
-				if not answers then
-					anstab = {"Yes", "No"}
-				else
-					for ans in answers:gmatch("([^,]+)") do
-						table.insert(anstab, ans)
-					end
-				end
-
-				local responses = {}
-				local players = service.GetPlayers(plr, args[1])
-
-				for i, v in players do
-					Routine(function()
-						local response = Remote.GetGui(v, "Vote", {Question = question; Answers = anstab;})
-						if response then
-							table.insert(responses, response)
-						end
-					end)
-				end
-
-				local t = 0
-				repeat wait(0.1) t=t+0.1 until t>=60 or #responses>=#players
-
-				local results = {}
-
-				for i, v in responses do
-					if not results[v] then results[v] = 0 end
-					results[v] = results[v]+1
-				end
-
-				local total = #responses
-				local tab = {
-					"Question: "..question;
-					"Total Responses: "..total;
-					"Didn't Vote: "..#players-total;
-				}
-				for i, v in anstab do
-					local ans = v
-					local num = results[v]
-					local percent
-					if not num then
-						num = 0
-						percent = 0
-					else
-						percent = math.floor((num/total)*100)
-					end
-
-					table.insert(tab, {Text=ans.." | "..percent.."% - "..num.."/"..total, Desc="Number: "..num.."/"..total.." | Percent: "..percent})
-				end
-				Remote.MakeGui(plr, "List", {Title = "Results"; Tab = tab;})--]]
 			end
 		};
 
