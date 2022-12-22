687ebc8f9fc785520fc80891c28bcb13d4717507
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2022-12-22 15:08:40 +0200

    Update Anti.lua

--- MainModule/Client/Core/Anti.lua
+++ MainModule/Client/Core/Anti.lua
@@ -228,6 +228,13 @@ return function(Vargs, GetEnv)
 				coroutine.wrap(function()
 					local LocalPlayer = service.UnWrap(Player)
 					local workspace = service.UnWrap(workspace)
+					local nilPlayers = setmetatable({}, {__mode = "v"})
+
+					service.UnWrap(Players).ChildRemoved:Connect(function(child)
+						if child:IsA("Player") then
+							table.insert(nilPlayers, child)
+						end
+					end)
 
 					local success, err = pcall(function()
 						LocalPlayer.Kick(workspace, "If this appears, you have a glitch. Method 1")
@@ -250,7 +257,7 @@ return function(Vargs, GetEnv)
 						for _, v in service.Players:GetPlayers() do
 							local otherPlayer = service.UnWrap(v)
 
-							if otherPlayer and otherPlayer.Parent == unwrappedPlayers and otherPlayer ~= LocalPlayer then
+							if otherPlayer and not table.find(nilPlayers, otherPlayer) and otherPlayer.Parent == unwrappedPlayers and otherPlayer ~= LocalPlayer then
 								local success, err = pcall(LocalPlayer.Kick, otherPlayer, "If this message appears, report it to Adonis maintainers. #2")
 								local success2, err2 = pcall(function()
 									otherPlayer:Kick("If this message appears, report it to Adonis maintainers. #3")
