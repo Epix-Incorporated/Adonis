e0185a9e69ae5777e221221facba53a0c29532c1
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2022-12-31 21:37:58 +0200

    Update Seize.client.lua

--- MainModule/Server/Dependencies/Assets/Seize.client.lua
+++ MainModule/Server/Dependencies/Assets/Seize.client.lua
@@ -4,10 +4,10 @@ local hum = char:FindFirstChildOfClass("Humanoid")
 local origvel = torso.Velocity
 local origrot = torso.RotVelocity
 repeat
- task.wait(0.1)
-  hum.PlatformStand = true
-  torso.Velocity = Vector3.new(math.random(-10,10),-5,math.random(-10,10))
-  torso.RotVelocity = Vector3.new(math.random(-5,5),math.random(-5,5),math.random(-5,5))
+	task.wait(0.1)
+	hum.PlatformStand = true
+	torso.Velocity = Vector3.new(math.random(-10,10),-5,math.random(-10,10))
+	torso.RotVelocity = Vector3.new(math.random(-5,5),math.random(-5,5),math.random(-5,5))
 until not torso or not hum
 
 hum.PlatformStand = false
