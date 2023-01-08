004a480009498807a34c93b2c085cce6b26d178e
xhayper <hayper1919@gmail.com>
2023-01-08 22:56:06 +0700

    feat: script optimization

--- MainModule/Server/Dependencies/Assets/Kitty.server.lua
+++ MainModule/Server/Dependencies/Assets/Kitty.server.lua
@@ -1,22 +1,22 @@
-while true do
+repeat
 	script.Parent.Texture = "http://www.roblox.com/asset/?id=280224764"
-	wait(0.1)
+	task.wait(0.1)
 	script.Parent.Texture = "http://www.roblox.com/asset/?id=280224790"
-	wait(0.1)
+	task.wait(0.1)
 	script.Parent.Texture = "http://www.roblox.com/asset/?id=280224800"
-	wait(0.1)
+	task.wait(0.1)
 	script.Parent.Texture = "http://www.roblox.com/asset/?id=280224820"
-	wait(0.1)
+	task.wait(0.1)
 	script.Parent.Texture = "http://www.roblox.com/asset/?id=280224830"
-	wait(0.1)
+	task.wait(0.1)
 	script.Parent.Texture = "http://www.roblox.com/asset/?id=280224844"
-	wait(0.1)
+	task.wait(0.1)
 	script.Parent.Texture = "http://www.roblox.com/asset/?id=280224861"
-	wait(0.1)
+	task.wait(0.1)
 	script.Parent.Texture = "http://www.roblox.com/asset/?id=280224899"
-	wait(0.1)
+	task.wait(0.1)
 	script.Parent.Texture = "http://www.roblox.com/asset/?id=280224924"
-	wait(0.1)
+	task.wait(0.1)
 	script.Parent.Texture = "http://www.roblox.com/asset/?id=280224955"
-	wait(0.1)
-end
+	task.wait(0.1)
+until false
