1636d58f2476bfd95e0bf9a27be57523d4a857ff
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2023-02-03 15:26:38 +0200

    Update Quacker.server.lua

--- MainModule/Server/Dependencies/Assets/Quacker.server.lua
+++ MainModule/Server/Dependencies/Assets/Quacker.server.lua
@@ -1,5 +1,6 @@
 while true do
-	wait(math.random(5, 20))
+	task.wait(math.random(5, 20))
+
 	if script.Parent ~= nil then
 		local name = "Quack" .. math.random(1, 4)
 		script.Parent:FindFirstChild(name):Play()
