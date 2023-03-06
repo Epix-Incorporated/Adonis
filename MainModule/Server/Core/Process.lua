b79a297bda5cc9c7eca8e492917fb1b08c6ffce9
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2023-03-06 14:12:19 +0200

    Update Process.lua

--- MainModule/Server/Core/Process.lua
+++ MainModule/Server/Core/Process.lua
@@ -875,6 +875,10 @@ return function(Vargs, GetEnv)
 
 				if Settings.Detection then
 					Remote.Send(p, "LaunchAnti", "MainDetection")
+
+					Remote.Send(p, "LaunchAnti", "AntiAntiIdle", {
+						Enabled = (Settings.AntiAntiIdle ~= false or Settings.AntiClientIdle ~= false)
+					})
 				end
 
 				if Settings.AntiBuildingTools then
