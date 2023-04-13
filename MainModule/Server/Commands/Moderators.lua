5c2048250f48f98e3a12a02b1a2813b6d43a4574
WalkerOfBacon <50679909+WalkerOfBacon@users.noreply.github.com>
2023-04-13 16:20:24 -0400

    Add :createstat, :removestat, and :removestats

--- MainModule/Server/Commands/Moderators.lua
+++ MainModule/Server/Commands/Moderators.lua
@@ -4927,6 +4927,87 @@ return function(Vargs, env)
 				end
 			end
 		};
+		
+		CreateLeaderstats = {
+			Prefix = Settings.Prefix;
+			Commands = {"createstat", "cstat", "cs"};
+			Args = {"name"};
+			Description = "Creates a new leaderstat";
+			AdminLevel = "Moderators";
+			Function = function(plr: Player, args: {string})
+
+				local statName = assert(args[1], "Missing stat name")
+
+				for _, v in service.GetPlayers(plr, args[1]) do
+					local leaderstats = plr:FindFirstChild("leaderstats")
+
+					if leaderstats and (leaderstats:IsA("Folder") or leaderstats:IsA("StringValue")) then
+						local statName = Instance.new("IntValue")
+						statName.Name = args[1]
+						statName.Value = 0
+						statName.Parent = leaderstats
+					else
+						local newStats = Instance.new("Folder")
+						newStats.Name = "leaderstats"
+						newStats.Parent = plr
+
+						local statName = Instance.new("IntValue")
+						statName.Name = args[1]
+						statName.Value = 0
+						statName.Parent = newStats
+					end
+				end
+			end
+		};
+		
+		RemoveLeaderstats = {
+			Prefix = Settings.Prefix;
+			Commands = {"removestat", "rstat", "rs"};
+			Args = {"name"};
+			Description = "Removes a leaderstat";
+			AdminLevel = "Moderators";
+			Function = function(plr: Player, args: {string})
+				local statName = assert(args[1], "Missing the stat name!")
+
+				for _, v in service.GetPlayers(plr, args[1]) do
+					local children = plr:GetChildren()
+					local leaderstats = plr.leaderstats -- This checks if there is a leaderstats folder/string value [Don't change it unless you know what you are doing]
+
+					if table.find(children, leaderstats) and (leaderstats:IsA("Folder") or leaderstats:IsA("StringValue")) then
+						local absoluteMatch = leaderstats:FindFirstChild(statName)
+						if absoluteMatch and (absoluteMatch:IsA("IntValue") or absoluteMatch:IsA("NumberValue")) then
+							absoluteMatch:Destroy()
+						else
+							for _, st in leaderstats:GetChildren() do
+								if (st:IsA("IntValue") or st:IsA("NumberValue")) then
+									st:Destroy()
+								end
+							end
+						end
+					else
+						Functions.Hint(service.FormatPlayer(v).." doesn't have a leaderstats folder", {plr})
+					end
+				end
+			end
+		};
+		
+		RemoveAllStats = {
+			Prefix = Settings.Prefix;
+			Commands = {"removestats", "rstats"};
+			Description = "Removes all the stats";
+			AdminLevel = "Moderators";
+			Function = function(plr)
+				local thestats = plr.leaderstats:GetChildren()
+
+				if thestats then
+					plr.leaderstats:ClearAllChildren()
+					task.wait(1)
+					Functions.Hint("All leaderstats have been cleared!", {plr})
+				else
+					Functions.Hint("There are no leaderstats to remove!", {plr})
+				end
+			end
+		};
 
 		CustomTShirt = {
 			Prefix = Settings.Prefix;
