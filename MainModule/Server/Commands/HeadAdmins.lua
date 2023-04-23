02f39b5cb69aa70e86a4f28b9a250707121afdb8
Alex Yerkes <alex@yerkes.dev>
2023-04-23 16:40:32 +0800

    Fix :awardbadge

--- MainModule/Server/Commands/HeadAdmins.lua
+++ MainModule/Server/Commands/HeadAdmins.lua
@@ -548,7 +548,8 @@ return function(Vargs, env)
 				local badgeId = assert(tonumber(args[2]), "Invalid badge ID specified!")
 				local badgeInfo = Variables.BadgeInfoCache[tostring(badgeId)]
 				if not badgeInfo then
-					local success, badgeInfo = nil, nil
+					local success
+					badgeInfo = nil
 					local tries = 0
 					repeat
 						tries += 1
@@ -577,6 +578,5 @@ return function(Vargs, env)
 					end
 				end
 			end
-		};
 	}
 end
