10a5730d7df5b2d6a26b4ecc20082125447a1a77
kaiserandaxl <122803145+kaiserandaxl@users.noreply.github.com>
2023-07-12 18:07:43 -0500

    Created Clip.client.lua

---
+++ MainModule/Server/Dependencies/Assets/Clip.client.lua
@@ -0,0 +1,11 @@
+local Humanoid = script.Parent.Humanoid
+
+for _,v in pairs(script.Parent:GetDescendants()) do
+	if v:IsA("BasePart") and (v.Name == "HumanoidRootPart" or v.Name == "UpperTorso" or v.Name == "LowerTorso" or v.Name == "Torso" or (v.Name == "Head" and script.Parent:FindFirstChild("Torso"))) then
+		v.CanCollide = true
+	end
+end
+
+Humanoid:ChangeState(18)
+task.wait()
+script:Destroy()
