29a11ebbdad37873a3a40f6ca9b65e3cd25eb2c1
Coasterteam <21298244+coasterteam@users.noreply.github.com>
2023-08-13 15:56:02 -0400

    Update Moderators.lua

--- MainModule/Server/Commands/Moderators.lua
+++ MainModule/Server/Commands/Moderators.lua
@@ -595,9 +595,21 @@ return function(Vargs, env)
 			Filter = true;
 			Description = "Makes a message in the target player(s)'s chat window";
 			AdminLevel = "Moderators";
-			Function = function(plr: Player, args: {string})
+			Function = function(plr: Player, args: {string}, data: {any})
 				for _, v in service.GetPlayers(plr, args[1]) do
-					Remote.Send(v, "Function", "ChatMessage", service.Filter(args[2], plr, v), Color3.fromRGB(255, 64, 77))
+					if service.TextChatService and service.TextChatService.ChatVersion == Enum.ChatVersion.TextChatService then
+						local TextToUse = args[2]
+						if data.Options.Chat ~= true then
+							TextToUse = server.Functions.EscapeRichTextTags(args[2] or "Hello world!")
+						end 
+						Remote.Send(
+							v, "Function", "DisplaySystemMessageInTextChat", nil, `<font color="rgb(255, 64, 77)">{
+							service.Filter(TextToUse, plr, v)
+							}</font>`)
+					else 
+						Remote.Send(v, "Function", "ChatMessage", service.Filter(args[2], plr, v), Color3.fromRGB(255, 64, 77))
+					end
+					
 				end
 			end
 		};
