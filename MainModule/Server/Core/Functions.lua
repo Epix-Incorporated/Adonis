a6a32ca0b867c8d1940708a0f916b4de1239a060
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2023-08-17 13:24:18 +0300

    Remove useless dublicate functions

--- MainModule/Server/Core/Functions.lua
+++ MainModule/Server/Core/Functions.lua
@@ -451,20 +451,6 @@ return function(Vargs, GetEnv)
 			return fakePlayer
 		end;
 
-		EscapeRichTextTags = function(text)
-			local EscapedText = {
-				["<"] = "&lt;";
-				[">"] = "&gt;";
-				['"'] = "&quot;";
-				["'"] =  "&apos;";
-				["%&"] = "&amp;";
-			}
-			for strToReplace,replacementStr in EscapedText do 
-				text = text:gsub(strToReplace, replacementStr)
-			end
-			return text
-		end;
-
 		GetChatService = function(waitTime)
 			local isTextChat = service.TextChatService.ChatVersion == Enum.ChatVersion.TextChatService
 			local chatHandler = service.ServerScriptService:WaitForChild("ChatServiceRunner", waitTime or isTextChat and 0.2 or 145)
@@ -1368,14 +1354,6 @@ return function(Vargs, GetEnv)
 			end
 
 			return arguments
-		end;
-
-		CountTable = function(tab)
-			local num = 0
-			for i in tab do
-				num += 1
-			end
-			return num
 		end;
 
 		IsValidTexture = function(id)
