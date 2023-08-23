c56624d9e6a0e8938ad6ea4a772abebd710907b9
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2023-08-23 19:21:01 +0300

    Make debugging detections easier

--- MainModule/Client/Core/Anti.lua
+++ MainModule/Client/Core/Anti.lua
@@ -48,8 +48,9 @@ return function(Vargs, GetEnv)
 	local Player = service.Players.LocalPlayer
 	local isStudio = select(2, pcall(service.RunService.IsStudio, service.RunService))
 	local Kick = Player.Kick
+	local isXbox = service.GuiService:IsTenFootInterface()
 	local isMobile = service.UserInputService.TouchEnabled and not service.UserInputService.KeyboardEnabled and not service.UserInputService.MouseEnabled
-	local hyperionEnabled = not service.GuiService:IsTenFootInterface() and not isMobile and (#tostring(tonumber(string.sub(tostring{}, 8))) > 10)
+	local hyperionEnabled = not isXbox and not isMobile and (#tostring(tonumber(string.sub(tostring{}, 8))) > 10)
 
 	local function Init()
 		UI = client.UI;
@@ -80,7 +81,7 @@ return function(Vargs, GetEnv)
 
 	local Detected = function(action, info, nocrash)
 		if NetworkClient and action ~= "_" then
-			pcall(Send, "D".."e".."t".."e".."c".."t".."e".."d", hyperionEnabled and "log" or action, tostring(info)..(hyperionEnabled and " (Hyperion is enabled, not giving punishment)" or ""))
+			pcall(Send, "D".."e".."t".."e".."c".."t".."e".."d", hyperionEnabled and "log" or action, tostring(info)..(hyperionEnabled and " - Hyperion is enabled, not giving punishment" or isXbox and " - On Xbox" or isMobile and " - On Mobile" or ""))
 			task.wait(0.5)
 			if not hyperionEnabled then
 				if action == "k".."i".."c".."k" then
