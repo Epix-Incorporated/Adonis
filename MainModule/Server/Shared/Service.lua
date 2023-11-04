2cf979a8c773d7e46031bea4d020d1e5fbf0dafe
Coasterteam <21298244+coasterteam@users.noreply.github.com>
2023-11-04 13:48:01 -0400

    Update Service.lua
    Add good signal to Shared Service

--- MainModule/Server/Shared/Service.lua
+++ MainModule/Server/Shared/Service.lua
@@ -257,94 +257,8 @@ return function(errorHandler, eventChecker, fenceSpecific, env)
 
 		GetEvent = function(name)
 			if not HookedEvents[name] then
-				local Wrap = service.Wrap
-				local UnWrap = service.UnWrap
-				local Wrapped = service.Wrapped
-				local WrapArgs = service.WrapEventArgs
-				local UnWrapArgs = service.UnWrapEventArgs
-				local event = Wrap(service.New("BindableEvent"), main)
-
-				event:SetSpecial("Wait", function(i, timeout)
-					local special = math.random()
-					local done = false
-					local ret
-
-					if timeout and type(timeout) == "number" and timeout > 0 then
-						Routine(function()
-							wait(timeout)
-							if not done then
-								UnWrap(event):Fire(special)
-							end
-						end)
-					end
-
-					repeat
-						ret = {UnWrap(event.Event):Wait()}
-					until ret[1] == 2 or ret[1] == special
-
-					done = true
-
-					if ret[1] == special then
-						warn(`Event waiter timed out [{timeout}]`)
-						return nil
-					else
-						return unpack(WrapArgs(ret), 2)
-					end
-				end)
-
-				event:SetSpecial("Fire", function(i, ...)
-					local packedResult = table.pack(...)
-					UnWrap(event):Fire(2, unpack(UnWrapArgs(packedResult), 1, packedResult.n))
-				end)
-
-				event:SetSpecial("ConnectOnce", function(i, func)
-					local event2; event2 = event:Connect(function(...)
-						event2:Disconnect()
-						func(...)
-					end)
-
-					return event2
-				end)
-
-				event:SetSpecial("Connect", function(i, func)
-					local special = math.random()
-					local event2 = Wrap(UnWrap(event.Event):Connect(function(con, ...)
-						local packedResult = table.pack(...)
-						if con == 2 or con == special then
-							func(unpack(WrapArgs(packedResult), 1, packedResult.n))
-						end
-					end), main)
-
-					event2:SetSpecial("Fire", function(i, ...)
-						local packedResult = table.pack(...)
-						UnWrap(event):Fire(special, unpack(UnWrapArgs(packedResult), 1, packedResult.n))
-					end)
-
-					event2:SetSpecial("Wait", function(i, timeout)
-						local ret
-
-						repeat
-							ret = {UnWrap(event.Event):Wait(timeout)}
-						until ret[1] == 2 or ret[1] == special
-
-						return unpack(WrapArgs(ret), 2)
-					end)
-
-					event2:SetSpecial("wait", event.Wait)
-					event2:SetSpecial("disconnect", event2.Disconnect)
-
-					return event2
-				end)
-
-				event:SetSpecial("fire", event.Fire)
-				event:SetSpecial("wait", event.Wait)
-				event:SetSpecial("connect", event.Connect)
-				event:SetSpecial("connectOnce", event.ConnectOnce)
-				event:SetSpecial("Event", service.Wrap(event.Event, main))
-				event.Event:SetSpecial("Wait", event.Wait)
-				event.Event:SetSpecial("wait", event.Wait)
-				event.Event:SetSpecial("Connect", event.Connect)
-				event.Event:SetSpecial("connect", event.Connect)
+				--// GoodSignal has been setup to be fully backwards-compatible with the existing Events system
+				local event = service.GoodSignal.new()
 
 				HookedEvents[name] = event
 				return event
@@ -1610,6 +1524,204 @@ return function(errorHandler, eventChecker, fenceSpecific, env)
 				changedLocale = translator.LocaleId
 			end)
 		end, warn)
+	end
+	
+	--// Add GoodSignal
+	do 
+		-- The currently idle thread to run the next handler on
+		local freeRunnerThread = nil
+
+		-- Function which acquires the currently idle handler runner thread, runs the
+		-- function fn on it, and then releases the thread, returning it to being the
+		-- currently idle one.
+		-- If there was a currently idle runner thread already, that's okay, that old
+		-- one will just get thrown and eventually GCed.
+		local function acquireRunnerThreadAndCallEventHandler(fn, ...)
+			local acquiredRunnerThread = freeRunnerThread
+			freeRunnerThread = nil
+			fn(...)
+			-- The handler finished running, this runner thread is free again.
+			freeRunnerThread = acquiredRunnerThread
+		end
+
+		-- Coroutine runner that we create coroutines of. The coroutine can be 
+		-- repeatedly resumed with functions to run followed by the argument to run
+		-- them with.
+		local function runEventHandlerInFreeThread()
+			-- Note: We cannot use the initial set of arguments passed to
+			-- runEventHandlerInFreeThread for a call to the handler, because those
+			-- arguments would stay on the stack for the duration of the thread's
+			-- existence, temporarily leaking references. Without access to raw bytecode
+			-- there's no way for us to clear the "..." references from the stack.
+			while true do
+				acquireRunnerThreadAndCallEventHandler(coroutine.yield())
+			end
+		end
+
+		-- Connection class
+		local Connection = {}
+		Connection.__index = Connection
+
+		function Connection.new(signal, fn)
+			return setmetatable({
+				_connected = true,
+				_signal = signal,
+				_fn = fn,
+				_next = false,
+			}, Connection)
+		end
+
+		function Connection:Disconnect()
+			self._connected = false
+
+			-- Unhook the node, but DON'T clear it. That way any fire calls that are
+			-- currently sitting on this node will be able to iterate forwards off of
+			-- it, but any subsequent fire calls will not hit it, and it will be GCed
+			-- when no more fire calls are sitting on it.
+			if self._signal._handlerListHead == self then
+				self._signal._handlerListHead = self._next
+			else
+				local prev = self._signal._handlerListHead
+				while prev and prev._next ~= self do
+					prev = prev._next
+				end
+				if prev then
+					prev._next = self._next
+				end
+			end
+		end
+
+		-- Make Connection strict
+		setmetatable(Connection, {
+			__index = function(tb, key)
+				error(("Attempt to get Connection::%s (not a valid member)"):format(tostring(key)), 2)
+			end,
+			__newindex = function(tb, key, value)
+				error(("Attempt to set Connection::%s (not a valid member)"):format(tostring(key)), 2)
+			end
+		})
+
+		-- Signal class
+		local Signal = {}
+		Signal.__index = Signal
+
+		function Signal.new()
+			return setmetatable({
+				_handlerListHead = false,
+				Event = {},
+			}, Signal)
+		end
+
+		function Signal:Connect(fn)
+			local connection = Connection.new(self, fn)
+			if self._handlerListHead then
+				connection._next = self._handlerListHead
+				self._handlerListHead = connection
+			else
+				self._handlerListHead = connection
+			end
+			local signalSelf = self
+			function connection:Fire(...)
+				signalSelf:Fire(fn)
+			end
+			function connection:fire(...)
+				signalSelf:Fire(fn)
+			end
+			function connection:disconnect()
+				self:Disconnect()
+			end
+			function connection:wait()
+				return signalSelf:Wait()
+			end
+			return connection
+		end
+		
+		function Signal:connect(fn)
+			return self:Connect(fn)
+		end
+		
+		-- Disconnect all handlers. Since we use a linked list it suffices to clear the
+		-- reference to the head handler.
+		function Signal:DisconnectAll()
+			self._handlerListHead = false
+		end
+
+		-- Signal:Fire(...) implemented by running the handler functions on the
+		-- coRunnerThread, and any time the resulting thread yielded without returning
+		-- to us, that means that it yielded to the Roblox scheduler and has been taken
+		-- over by Roblox scheduling, meaning we have to make a new coroutine runner.
+		function Signal:Fire(...)
+			local item = self._handlerListHead
+			while item do
+				if item._connected then
+					if not freeRunnerThread then
+						freeRunnerThread = coroutine.create(runEventHandlerInFreeThread)
+						-- Get the freeRunnerThread to the first yield
+						coroutine.resume(freeRunnerThread)
+					end
+					task.spawn(freeRunnerThread, item._fn, ...)
+				end
+				item = item._next
+			end
+		end
+		
+		function Signal:fire(...)
+			self:Fire(...)
+		end
+
+		-- Implement Signal:Wait() in terms of a temporary connection using
+		-- a Signal:Connect() which disconnects itself.
+		function Signal:Wait()
+			local waitingCoroutine = coroutine.running()
+			local cn;
+			cn = self:Connect(function(...)
+				cn:Disconnect()
+				task.spawn(waitingCoroutine, ...)
+			end)
+			return coroutine.yield()
+		end
+		
+		function Signal:wait()
+			return self:Wait()
+		end
+		
+		-- Implement Signal:Once() in terms of a connection which disconnects
+		-- itself before running the handler.
+		function Signal:Once(fn)
+			local cn;
+			cn = self:Connect(function(...)
+				if cn._connected then
+					cn:Disconnect()
+				end
+				fn(...)
+			end)
+			return cn
+		end
+		
+		function Signal:ConnectOnce(fn)
+			return self:Once(fn)
+		end
+		
+		function Signal:connectOnce(fn)
+			return self:Once(fn)
+		end
+		
+		function Signal:Destroy()
+			self:DisconnectAll()
+			setmetatable(self, nil)
+		end
+
+		-- Make signal strict
+		setmetatable(Signal, {
+			__index = function(tb, key)
+				error(("Attempt to get Signal::%s (not a valid member)"):format(tostring(key)), 2)
+			end,
+			__newindex = function(tb, key, value)
+				error(("Attempt to set Signal::%s (not a valid member)"):format(tostring(key)), 2)
+			end
+		})
+
+		service.GoodSignal = Signal
 	end
 
 	return service
