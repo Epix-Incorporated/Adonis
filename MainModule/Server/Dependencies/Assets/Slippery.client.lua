a96b6abae24cc4293f935888ed56a4282ae2e133
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2023-11-12 15:24:48 +0200

    Update Slippery.client.lua

--- MainModule/Server/Dependencies/Assets/Slippery.client.lua
+++ MainModule/Server/Dependencies/Assets/Slippery.client.lua
@@ -2,7 +2,7 @@ task.wait(0.5)
 local vel = script.Parent:WaitForChild("ADONIS_IceVelocity")
 
 while script.Parent ~= nil and vel and vel.Parent  ~= nil do 
-	vel.AssemblyLinearVelocity = Vector3.new(script.Parent.AssemblyLinearVelocity.X, 0, script.Parent.AssemblyLinearVelocity.Z)
+	vel.Velocity = Vector3.new(script.Parent.AssemblyLinearVelocity.X, 0, script.Parent.AssemblyLinearVelocity.Z)
 	task.wait(0.1)
 end
 
