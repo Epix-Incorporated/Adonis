790e38c7d61fe84184fade41da54c4365674f178
Dimenpsyonal <64012878+Dimenpsyonal@users.noreply.github.com>
2023-11-20 12:09:28 +0000

    Update FlyClipper.client.lua

--- MainModule/Server/Dependencies/Assets/FlyClipper.client.lua
+++ MainModule/Server/Dependencies/Assets/FlyClipper.client.lua
@@ -2,14 +2,14 @@ task.wait()
 local players = game:GetService("Players")
 local localplayer = players.LocalPlayer
 local torso = localplayer.Character.HumanoidRootPart 
-local hum = localplayer.Character:FindFirstChildOfClass("Humanoid")
+local hum = localplayer.Character.Humanoid
 local mouse = localplayer:GetMouse()
 local enabled = script.Enabled
 local running = true
 local dir = {w = 0, s = 0, a = 0, d = 0} 
 local spd = 2 
-	
-local moos = mouse.KeyDown:Connect(function(key)
+
+local moos = mouse.KeyDown:connect(function(key)
 	if key:lower() == "w" then 
 		dir.w = 1 
 	elseif key:lower() == "s" then 
@@ -25,7 +25,7 @@ local moos = mouse.KeyDown:Connect(function(key)
 	end 
 end) 
 
-local moos1 = mouse.KeyUp:Connect(function(key)
+local moos1 = mouse.KeyUp:connect(function(key)
 	if key:lower() == "w" then 
 		dir.w = 0 
 	elseif key:lower() == "s" then 
@@ -36,10 +36,10 @@ local moos1 = mouse.KeyUp:Connect(function(key)
 		dir.d = 0 
 	end 
 end) 
-					
+
 torso.Anchored = true 
 hum.PlatformStand = true 
-local macka = hum.Changed:Connect(function() 
+local macka = hum.Changed:connect(function() 
 	hum.PlatformStand = true 
 end) 
 
@@ -48,12 +48,12 @@ repeat
 		running = false
 		break
 	end
-	task.wait()
-	torso.CFrame = CFrame.new(torso.Position, workspace.CurrentCamera.CFrame.p) * CFrame.Angles(0,math.rad(180),0) * CFrame.new((dir.d-dir.a)*spd,0,(dir.s-dir.w)*spd) 
+	wait(1/60)
+	torso.CFrame = CFrame.new(torso.Position, workspace.CurrentCamera.CoordinateFrame.p) * CFrame.Angles(0,math.rad(180),0) * CFrame.new((dir.d-dir.a)*spd,0,(dir.s-dir.w)*spd) 
 until not running or hum.Parent == nil or torso.Parent == nil or script.Parent == nil or not enabled or not enabled.Value or not enabled.Parent
 
-moos:Disconnect()
-moos1:Disconnect()
-macka:Disconnect()
+moos:disconnect()
+moos1:disconnect()
+macka:disconnect()
 torso.Anchored = false
 hum.PlatformStand = false
