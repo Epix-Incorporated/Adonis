05116b005ca4f2974753c4b74d61620dac417464
Dimenpsyonal <64012878+Dimenpsyonal@users.noreply.github.com>
2023-11-20 13:44:51 +0000

    Revert flyclipper changes

--- MainModule/Server/Dependencies/Assets/FlyClipper.client.lua
+++ MainModule/Server/Dependencies/Assets/FlyClipper.client.lua
@@ -1,59 +1,46 @@
-task.wait()
 local players = game:GetService("Players")
 local localplayer = players.LocalPlayer
 local torso = localplayer.Character.HumanoidRootPart 
-local hum = localplayer.Character.Humanoid
+local hum = localplayer.Character:FindFirstChildOfClass("Humanoid")
 local mouse = localplayer:GetMouse()
 local enabled = script.Enabled
 local running = true
 local dir = {w = 0, s = 0, a = 0, d = 0} 
 local spd = 2 
-
-local moos = mouse.KeyDown:connect(function(key)
+	
+local moos = mouse.KeyDown:Connect(function(key)
 	if key:lower() == "w" then 
 		dir.w = 1 
 	elseif key:lower() == "s" then 
-		dir.s = 1 
-	elseif key:lower() == "a" then 
-		dir.a = 1 
-	elseif key:lower() == "d" then 
-		dir.d = 1 
-	elseif key:lower() == "q" then 
-		spd = spd + 1 
-	elseif key:lower() == "e" then 
-		spd = spd - 1 
+	@@ -25,7 +25,7 @@ local moos = mouse.KeyDown:Connect(function(key)
 	end 
 end) 
 
-local moos1 = mouse.KeyUp:connect(function(key)
+local moos1 = mouse.KeyUp:Connect(function(key)
 	if key:lower() == "w" then 
 		dir.w = 0 
 	elseif key:lower() == "s" then 
-		dir.s = 0 
-	elseif key:lower() == "a" then 
-		dir.a = 0 
-	elseif key:lower() == "d" then 
+	@@ -36,10 +36,10 @@ local moos1 = mouse.KeyUp:Connect(function(key)
 		dir.d = 0 
 	end 
 end) 
-
+					
 torso.Anchored = true 
 hum.PlatformStand = true 
-local macka = hum.Changed:connect(function() 
+local macka = hum.Changed:Connect(function() 
 	hum.PlatformStand = true 
 end) 
 
-repeat 
-	if enabled == nil or enabled.Parent == nil or enabled.Value == false then
+	@@ -48,12 +48,12 @@ repeat
 		running = false
 		break
 	end
-	wait(1/60)
-	torso.CFrame = CFrame.new(torso.Position, workspace.CurrentCamera.CoordinateFrame.p) * CFrame.Angles(0,math.rad(180),0) * CFrame.new((dir.d-dir.a)*spd,0,(dir.s-dir.w)*spd) 
+	task.wait()
+	torso.CFrame = CFrame.new(torso.Position, workspace.CurrentCamera.CFrame.p) * CFrame.Angles(0,math.rad(180),0) * CFrame.new((dir.d-dir.a)*spd,0,(dir.s-dir.w)*spd) 
 until not running or hum.Parent == nil or torso.Parent == nil or script.Parent == nil or not enabled or not enabled.Value or not enabled.Parent
 
-moos:disconnect()
-moos1:disconnect()
-macka:disconnect()
+moos:Disconnect()
+moos1:Disconnect()
+macka:Disconnect()
 torso.Anchored = false
 hum.PlatformStand = false
