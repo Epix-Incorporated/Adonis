eb02062b81490b7d63e4ae73cfc5c1d3f6c319b1
Dimenpsyonal <64012878+Dimenpsyonal@users.noreply.github.com>
2023-12-06 12:01:53 +0000

    Update Credits

--- MainModule/Server/Shared/Credits.lua
+++ MainModule/Server/Shared/Credits.lua
@@ -78,9 +78,10 @@ return {
 		{Text = "@bigbenster702",	Desc = "Open Source Contributor"};
 		{Text = "@WalkerOfBacon", 	Desc = "Open Source Contributor"};
 		{Text = "@wilsontulus", 	Desc = "Open Source Contributor"};
-		{Text = "RealEthanPlayzDev", Desc = "Open Source Contributor"};
-		{Text = "Redfuire", Desc = "Open Source Contributor"};
-		{Text = "RadiantUwU", Desc = "Open Source Contributor"};
+		{Text = "@RealEthanPlayzDev", Desc = "Open Source Contributor"};
+		{Text = "@Redfuire", Desc = "Open Source Contributor"};
+		{Text = "@RadiantUwU", Desc = "Open Source Contributor"};
+		{Text = "@kaiserandaxl", Desc = "Open Source Contributor"};
 	};
 
 	Misc = {
