a8dc30e56dc55aecc1846ed59cf3b699e2c310ef
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2024-01-10 17:33:54 +0200

    Add legacy aliases of player points

--- MainModule/Server/Plugins/Misc_Features.lua
+++ MainModule/Server/Plugins/Misc_Features.lua
@@ -49,6 +49,15 @@ return function(Vargs, GetEnv)
 		end;
 		return table.concat(Res)
 	end;
+	for k, v in {-- Legacy aliases
+		[":giveppoints <player> <amount>"] = ":script game:GetService(\"PointsService\"):AwardPoints(game:GetService:(\"Players\"):FindFirstChild(\"<player>\").UserId, <points>)",
+		[":giveplayerpoints <player> <amount>"] = ":script game:GetService(\"PointsService\"):AwardPoints(game:GetService:(\"Players\"):FindFirstChild(\"<player>\").UserId, <points>)",
+		[":sendplayerpoints <player> <amount>"] = ":script game:GetService(\"PointsService\"):AwardPoints(game:GetService:(\"Players\"):FindFirstChild(\"<player>\").UserId, <points>)",
+	} do
+		if not Variables.Aliases[k] then
+			Variables.Aliases[k] = v
+		end
+	end
 
 	Logs:AddLog("Script", "Misc Features Module Loaded")
 end
