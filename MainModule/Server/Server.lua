9c88e41dc9363f715f007ce94b8b37c706ffaaab
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2024-01-14 14:03:03 +0200

    Make disabling linkedlists a loader flag (#1335)
    * Change linkedlist toggle
    
    * Removed linkedlist toggle from settings
    
    * Make disabling linkedlists a loader flag
    
    * Update Logs.lua

--- MainModule/Server/Server.lua
+++ MainModule/Server/Server.lua
@@ -553,6 +553,7 @@ return service.NewProxy({
 		server.LoadModule = LoadModule
 		server.LoadPackage = LoadPackage
 		server.ServiceSpecific = ServiceSpecific
+		server.DisableLinkedListsInLogs = data.DisableLinkedListsInLogs
 
 		server.Shared = Folder.Shared
 		server.ServerPlugins = data.ServerPlugins
