76eb69261b6f40e08aa7925ac14268914da2a09d
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2024-01-16 13:33:25 +0200

    Change to use Core.Loadstring

--- MainModule/Server/Server.lua
+++ MainModule/Server/Server.lua
@@ -98,12 +98,6 @@ local ServiceSpecific = {}
 local oldReq = require
 local Folder = script.Parent
 local oldInstNew = Instance.new
-local loadstringEnabled = pcall(loadstring, "return 1")
-local customLoadstring = (function()
-	local module, fione = script.Parent.Dependencies.Loadstring:Clone(), script.Parent.Shared.FiOne:Clone()
-	fione.Parent = module
-	return require(module)
-end)()
 local isModule = function(module)
 	for ind, modu in pairs(server.Modules) do
 		if module == modu then
@@ -201,18 +195,19 @@ end
 local function LoadModule(module, yield, envVars, noEnv, isCore)
 	noEnv = false --// Seems to make loading take longer when true (?)
 	local isFunc = type(module) == "function"
+	local isRaw = type(module) == "string"
 	local module = (isFunc and service.New("ModuleScript", {Name = "Non-Module Loaded"})) or module
-	local plug = (isFunc and module) or type(module) == "string" and assert(loadstringEnabled and loadstring(module) or customLoadstring(module, GetEnv({}, envVars)), "Failed to compile module")() or require(module)
+	local plug = (isFunc and module) or isRaw and assert(assert(Core.Loadstring, "Cannot compile plugin due to Core.Loadstring missing")(module, GetEnv({}, envVars)), "Failed to compile module")() or require(module)
 
-	if server.Modules and type(module) ~= "function" and type(module) ~= "string" then
+	if server.Modules and not isFunc and not isRaw then
 		table.insert(server.Modules,module)
 	end
 
-	if type(plug) == "function" then
+	if isFunc then
 		if isCore then
 			local ran, err = service.TrackTask(
 				`CoreModule: {module}`,
-				((noEnv or not loadstringEnabled) and plug) or setfenv(plug, GetEnv(getfenv(plug), envVars)),
+				((noEnv or isRaw) and plug) or setfenv(plug, GetEnv(getfenv(plug), envVars)),
 				function(err)
 					warn(`Module encountered an error while loading: {module}\n{err}\n{debug.traceback()}`)
 				end,
@@ -231,7 +226,7 @@ local function LoadModule(module, yield, envVars, noEnv, isCore)
 			--service.Threads.RunTask(`PLUGIN: {module}`,setfenv(plug,GetEnv(getfenv(plug), envVars)))
 			local ran, err = service.TrackTask(
 				`Plugin: {module}`,
-				(noEnv and plug) or setfenv(plug, GetEnv(getfenv(plug), envVars)),
+				((noEnv or isRaw) and plug) or setfenv(plug, GetEnv(getfenv(plug), envVars)),
 				function(err)
 					warn(`Module encountered an error while loading: {module}\n{err}\n{debug.traceback()}`)
 				end,
