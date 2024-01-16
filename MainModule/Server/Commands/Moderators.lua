1e767c9b477d2dbf940ff2ec8dd8cad5e0a96dd0
kaiserandaxl <122803145+kaiserandaxl@users.noreply.github.com>
2024-01-15 19:14:23 -0600

    Update Moderators.lua

--- MainModule/Server/Commands/Moderators.lua
+++ MainModule/Server/Commands/Moderators.lua
@@ -2737,7 +2737,9 @@ return function(Vargs, env)
 						service.TrackTask(`Thread: JailLoop{ind}`, function()
 							while task.wait() and Variables.Jails[ind] == jail and Model.Parent == workspace do
 								if Variables.Jails[ind] == jail and service.Players:FindFirstChild(jail.Name) then
-									v = service.Players:FindFirstChild(jail.Name)
+									if Settings.ReJail then
+										v = service.Players:FindFirstChild(jail.Name)
+									end
 									if Color == "rainbow" then
 										box.Color3 = Color3.fromHSV(tick()%5/5, 1, 1)
 									end
