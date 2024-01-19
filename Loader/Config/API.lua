0014abe6ec421dbd37ad24c1632314d02cbf9b82
Dimenpsyonal <64012878+Dimenpsyonal@users.noreply.github.com>
2024-01-19 12:00:43 +0000

    Update API to include GitHub link and some info

--- Loader/Config/API.lua
+++ Loader/Config/API.lua
@@ -1,3 +1,8 @@
 --[[
-	The Adonis API provides useful information on Adonis, its functions, and more.
+	If you're learning how to create in Adonis, the wiki may be of help.
+	The wiki contains informationa about the Adonis API and how to use it;
+	as well as information about settings and how to use Adonis.
+
+	It can be viewed here: 
+		https://github.com/Epix-Incorporated/Adonis/wiki
 --]]
