f1576e57db7cad119dd1b137c119cb79198869bf
Dimenpsyonal <64012878+Dimenpsyonal@users.noreply.github.com>
2024-03-08 11:12:07 +0000

    Replace Arial

--- MainModule/Client/UI/Default/YesNoPrompt.lua
+++ MainModule/Client/UI/Default/YesNoPrompt.lua
@@ -44,7 +44,7 @@ return function(data, env)
 
 	local no = window:Add("TextButton", {
 		Text = "No";
-		Font = Enum.Font.Arial;
+		Font = Enum.Font.SourceSans;
 		TextSize = 18;
 		AnchorPoint = Vector2.new(0, 1);
 		Size = UDim2.new(0.5, -7, 0, 30);
