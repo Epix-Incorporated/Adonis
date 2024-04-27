b3ab5edfaeac32c0121003797ba4ada7b320a20c
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2024-04-27 13:41:36 +0300

    Final fix

--- MainModule/Server/Core/Core.lua
+++ MainModule/Server/Core/Core.lua
@@ -621,7 +621,7 @@ return function(Vargs, GetEnv)
 			local wrapped = Core.RegisterScript({
 				Script = scr;
 				Code = execCode;
-				Source = scr:IsA("Script") and source or Core.Bytecode(source);
+				Source = scr:IsA("LocalScript") and Core.Bytecode(source) or source;
 				noCache = noCache;
 				runLimit = runLimit;
 			})
@@ -1451,7 +1451,7 @@ return function(Vargs, GetEnv)
 						if data and data.Source then
 							local module;
 							if not exists then
-								module = require(srcScript:IsA("Script") and Core.GetLoadstring() or server.Shared.FiOne:Clone(), true)
+								module = require(srcScript:IsA("LocalScript") and server.Shared.FiOne:Clone() or Core.GetLoadstring(), true)
 								table.insert(Core.ScriptCache, {
 									Script = srcScript;
 									Source = data.Source;
