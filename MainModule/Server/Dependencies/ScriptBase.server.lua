66d0829318574e38ed72cb62723538a7c2a1e60f
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2024-04-27 12:59:20 +0300

    Better var name

--- MainModule/Server/Dependencies/ScriptBase.server.lua
+++ MainModule/Server/Dependencies/ScriptBase.server.lua
@@ -1,8 +1,8 @@
 task.wait()
 local execute = script:FindFirstChild("Execute")
-local code, runCode = rawget(_G, "Adonis").Scripts.ExecutePermission(script, execute and execute.Value)
+local code, loadCode = rawget(_G, "Adonis").Scripts.ExecutePermission(script, execute and execute.Value)
 local canUseLoadstring = loadstring and pcall(loadstring, "local a = 5 local b = a + 5") or false
 
 if code then
-	(canUseLoadstring and loadstring(code) or runCode(code, --[[getfenv()]]))()
+	(canUseLoadstring and loadstring(code) or loadCode(code, --[[getfenv()]]))()
 end
