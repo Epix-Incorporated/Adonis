0f8d4b0d5c88e8ba6597cc472aafd10734010ca0
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2024-04-27 12:59:11 +0300

    Better var name

--- MainModule/Server/Dependencies/LocalScriptBase.client.lua
+++ MainModule/Server/Dependencies/LocalScriptBase.client.lua
@@ -1,8 +1,8 @@
 task.wait()
 local execute = script:FindFirstChild("Execute")
-local code, runCode = rawget(_G, "Adonis").Scripts.ExecutePermission(script, execute and execute.Value)
+local code, loadCode = rawget(_G, "Adonis").Scripts.ExecutePermission(script, execute and execute.Value)
 local env = getfenv()
 
 if code then
-	runCode(code, env)()
+	loadCode(code, env)()
 end
