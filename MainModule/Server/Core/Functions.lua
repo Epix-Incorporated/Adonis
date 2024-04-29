b30cf56e7ab6ce8562f6cdb1734ddb71e556ff8f
Bargain Bucket <122705147+NoobBucket@users.noreply.github.com>
2024-04-29 09:05:26 +0100

    Fix duration time in Functions.lua (#1484)
    * Update Functions.lua
    
    * Add duration check in Functions.lua

--- MainModule/Server/Core/Functions.lua
+++ MainModule/Server/Core/Functions.lua
@@ -880,7 +880,7 @@ return function(Vargs, GetEnv)
 		end;
 
 		Hint = function(message, players, duration, title, image)
-			duration = duration or (#tostring(message) / 19 + 2.5)
+			duration = duration or (#tostring(message) / 19) + 2.5
 
 			for _, v in players do
 				Remote.MakeGui(v, "Hint", {
@@ -965,6 +965,8 @@ return function(Vargs, GetEnv)
 
 		Notification = function(title, message, players, duration, icon, onClick)
 			icon = icon and icon:match("MatIcon://(.+)") or icon
+			duration = duration or (#tostring(message) / 19) + 2.5
+			
 			for _, v in players do
 				Remote.MakeGui(v, "Notification", {
 					Title = title;
