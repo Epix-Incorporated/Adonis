7636669f60acd4a198b75c703fb492dd44f64247
ocelot81 <128096274+ocelot81@users.noreply.github.com>
2024-04-30 23:47:16 +0200

    Update Core.lua

--- MainModule/Server/Core/Core.lua
+++ MainModule/Server/Core/Core.lua
@@ -968,7 +968,7 @@ return function(Vargs, GetEnv)
 							return nil
 						end
 						--// Prevent loading from DB to Trello ranks
-						if curName:match("Trello") and curTable and curTable.IsExternal then
+						if string.match(curName, "Trello") and curTable and curTable.IsExternal then
 							return nil
 						end
 					end
@@ -1199,7 +1199,7 @@ return function(Vargs, GetEnv)
 							Core.WarnedAboutAdminsLoadingWhenSaveAdminsIsOff = true
 						end
 						--// No adding to Trello or WebPanel rank users list via Datastore
-						if type(indList[3]) == 'string' and (indList[3]:match("Trello") or indList[3]:match("WebPanel")) then
+						if type(indList[3]) == 'string' and (string.match(indList[3], "Trello") or string.match(indList[3], "WebPanel")) then
 							return
 						end
 					end
