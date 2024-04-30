127296cab4924c2698053e4f8fae582075a9eae2
ocelot81 <128096274+ocelot81@users.noreply.github.com>
2024-04-30 22:20:29 +0200

    Update Functions.lua

--- MainModule/Server/Core/Functions.lua
+++ MainModule/Server/Core/Functions.lua
@@ -582,7 +582,7 @@ return function(Vargs, GetEnv)
 							--// Check for usernames
 							for _, v in parent:GetChildren() do
 								local p = getplr(v)
-								if p and p.ClassName == "Player" and string.match(string.lower(p.Name), `^{service.SanitizePattern(string.lower(s)}`) then
+								if p and p.ClassName == "Player" and string.match(string.lower(p.DisplayName), `^{service.SanitizePattern(string.lower(s))}`) then
 									table.insert(players, p)
 									plus()
 								end
