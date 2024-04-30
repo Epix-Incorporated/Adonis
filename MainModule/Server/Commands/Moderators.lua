2de89e4ec24e4adf0391c299711617d91eea1d9a
WalkerOfBacon <50679909+WalkerOfBacon@users.noreply.github.com>
2024-04-30 04:03:55 -0400

    Change command aliases for ShowTrelloBanList (#1485)
    * Change command aliases for ShowTrelloBanList
    
    it makes no sense why all of them are capital
    
    * Add back old aliases
    
    * Uncapitalise
    
    ---------
    
    Co-authored-by: Dimenpsyonal <64012878+Dimenpsyonal@users.noreply.github.com>

--- MainModule/Server/Commands/Moderators.lua
+++ MainModule/Server/Commands/Moderators.lua
@@ -1196,7 +1196,7 @@ return function(Vargs, env)
 
 		ShowTrelloBansList = {
 			Prefix = Settings.Prefix;
-			Commands = {"SyncedTrelloBans", "TrelloBans", "TrelloBanList", "ShowTrelloBans"};
+			Commands = {"trellobans", "trellobanlist", "tbl", "syncedtrellobans", "showtrellobans"};
 			Args = {};
 			Description = "Shows bans synced from Trello.";
 			TrelloRequired = true;
