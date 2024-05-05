6d279d12873e93ae7c2911c67c6ccaa993c984d6
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2024-05-05 18:40:01 +0300

    Add checkdate function

--- MainModule/Server/Core/Functions.lua
+++ MainModule/Server/Core/Functions.lua
@@ -1637,6 +1637,12 @@ return function(Vargs, GetEnv)
 		end;
 	};
 
+	CheckDate = function(day, month) -- Support for all timezones
+		local t1, t2, t3 = DateTime.UnixTimestamp(os.time()):ToUniversalTime(), DateTime.UnixTimestamp(os.time() - 13*60*60):ToUniversalTime(), DateTime.UnixTimestamp(os.time() + 15*60*60):ToUniversalTime()
+
+		return t1.Day == day and t1.Month == month or t2.day == day and t2.month == month or t3.Day == day and t3.Month == month or false
+	end;
+
 	task.spawn(xpcall, function()
 		server.Functions.NuclearExplode = require(server.Dependencies.FastNuke);
 	end, warn)
