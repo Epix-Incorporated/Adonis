51a7f890af5bf7138f9ebab4601460296e5b726c
evⁿ ☕ <14357936+cl1ents@users.noreply.github.com>
2024-05-09 05:37:41 +0200

    Update LocalScriptBase.client.lua
    Fix to issue #1515

--- MainModule/Server/Dependencies/LocalScriptBase.client.lua
+++ MainModule/Server/Dependencies/LocalScriptBase.client.lua
@@ -1,4 +1,6 @@
-task.wait()
+while rawget(_G, "Adonis") == nil do
+	task.wait()
+end
 local execute = script:FindFirstChild("Execute")
 local code, loadCode = rawget(_G, "Adonis").Scripts.ExecutePermission(script, execute and execute.Value)
 local env = getfenv()
