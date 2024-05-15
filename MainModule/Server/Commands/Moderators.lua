74ca2ff53bcff5a9341091b8c2a53bc94107b2ba
Spikers <37247310+Sqikerz@users.noreply.github.com>
2024-05-15 04:08:00 -0400

    Add new :freecam console keybinds to instructions

--- MainModule/Server/Commands/Moderators.lua
+++ MainModule/Server/Commands/Moderators.lua
@@ -6858,7 +6858,7 @@ return function(Vargs, env)
 			Prefix = Settings.Prefix;
 			Commands = {"freecam"};
 			Args = {"player"};
-			Description = "Makes it so the target player(s)'s cam can move around freely (Press Space or Shift+P to toggle freecam)";
+			Description = "Makes it so the target player(s)'s cam can move around freely (Press Shift+P, F, or DPadLeft to toggle freecam)";
 			AdminLevel = "Moderators";
 			Function = function(plr: Player, args: {string})
 				for _, v in service.GetPlayers(plr, args[1]) do
@@ -6873,7 +6873,7 @@ return function(Vargs, env)
 					freecam.ResetOnSpawn = false
 					freecam.Freecam.Disabled = false
 					freecam.Parent = plrgui
-					Functions.Notification("Notification", "Freecam has been enabled. Press Shift+P, or F to toggle freecam on or off.", {v}, 15)
+					Functions.Notification("Notification", "Freecam has been enabled. Press Shift+P, F, or DPadLeft to toggle freecam on or off.", {v}, 15)
 				end
 			end
 		};
