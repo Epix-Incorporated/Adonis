84086952bc21fe51aad53af03e35f4615ccd329d
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2024-10-14 13:50:55 +0300

    Fix selene lint

--- MainModule/Server/Dependencies/Assets/Sfling.client.lua
+++ MainModule/Server/Dependencies/Assets/Sfling.client.lua
@@ -7,11 +7,12 @@ local strength = script:WaitForChild("Strength").Value
 for i = 1, 100 do
 	task.wait(0.1)
 	humanoid.Sit = true
-	local ex = Instance.new("Explosion", cam)
+	local ex = Instance.new("Explosion")
 	ex.Position = torso.Position + Vector3.new(math.random(-5, 5), -10, math.random(-5, 5))
 	ex.BlastRadius = 35
 	ex.BlastPressure = strength
 	ex.ExplosionType = Enum.ExplosionType.Craters
 	ex.DestroyJointRadiusPercent = 0
+	ex.Parent = cam
 end
 script:Destroy()
