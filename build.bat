8bcb039a4a88459d76ef5a0f077981d4704aee7d
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2024-11-22 17:20:20 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,6 @@
 @echo off
 SETLOCAL
 
-:: Backup if selene is not globally installed
 where selene >nul 2>nul
 IF %ERRORLEVEL% NEQ 0 (
 	SET SELENE_COMMAND=.\selene.exe
@@ -9,7 +8,6 @@ IF %ERRORLEVEL% NEQ 0 (
 	SET SELENE_COMMAND=selene
 )
 
-:: Backup if rojo is not globally installed
 where rojo >nul 2>nul
 IF %ERRORLEVEL% NEQ 0 (
 	SET ROJO_COMMAND=.\rojo.exe
@@ -17,11 +15,9 @@ IF %ERRORLEVEL% NEQ 0 (
 	SET ROJO_COMMAND=rojo
 )
 
-:: Run selene linter for loader and mainmodule
-echo Running %SELENE_COMMAND% ./MainModule ./Loader
+echo Checking for lint errors with %SELENE_COMMAND% from ./Loader and ./MainModule 
 %SELENE_COMMAND% ./MainModule ./Loader
 
-:: Run rojo build
 echo Running %ROJO_COMMAND% build -o Adonis.rbxm
 %ROJO_COMMAND% build -o Adonis.rbxm
 
